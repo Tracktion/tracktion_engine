e7ae31aa0fe43cc79b8e41d60361e1c556496e18
David Rowland <drowland96@gmail.com>
2018-11-18 13:06:19 +0000

    Added travis.yml

--- tests/windows/build_examples.bat
+++ tests/windows/build_examples.bat
@@ -9,6 +9,9 @@ set TRACKTION_ENGINE_DIR=%ROOT%/modules
 set PROJUCER_DIR=%JUCE_DIR%/extras/Projucer
 set PROJUCER_EXE=%PROJUCER_DIR%/Builds/VisualStudio2017/x64/Release/App/Projucer.exe
 
+if not defined MSBUILD_EXE set MSBUILD_EXE=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
+echo MSBUILD_EXE: "%MSBUILD_EXE%"
+
 ::============================================================
 ::   Build Projucer
 ::============================================================
@@ -33,5 +36,5 @@ exit /B %ERRORLEVEL%
     call "%PROJUCER_EXE%" --create-project-from-pip "%EXAMPLE_PIP_FILE%" "%EXAMPLE_DEST_DIR%" "%JUCE_DIR%/modules" "%TRACKTION_ENGINE_DIR%"
     call "%PROJUCER_EXE%" --resave "%EXAMPLE_ROOT_DIR%/%EXAMPLE_NAME%.jucer"
     cd "%EXAMPLE_ROOT_DIR%/Builds/VisualStudio2017"
-    "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" %EXAMPLE_NAME%.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 /p:TreatWarningsAsErrors=true
+    "%MSBUILD_EXE%" %EXAMPLE_NAME%.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 /p:TreatWarningsAsErrors=true
 exit /B 0
--- tests/windows/build_projucer.bat
+++ tests/windows/build_projucer.bat
@@ -3,6 +3,8 @@
 cd "%~dp0%../.."
 set ROOT=%cd%
 
+if not defined MSBUILD_EXE set MSBUILD_EXE=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
+
 ::=========================================================
 echo "Building Projucer"
 ::=========================================================
@@ -10,5 +12,5 @@ set PROJUCER_ROOT=%ROOT%/modules/juce/extras/Projucer/Builds/VisualStudio2017
 set PROJUCER_EXE=%PROJUCER_ROOT%/x64/Release/App/Projucer.exe
 cd "%PROJUCER_ROOT%"
 set CL=/DJUCER_ENABLE_GPL_MODE
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Projucer.sln /p:VisualStudioVersion=15.0 /m /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64
+"%MSBUILD_EXE%" Projucer.sln /p:VisualStudioVersion=15.0 /m /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64
 if not exist "%PROJUCER_EXE%" exit 1
