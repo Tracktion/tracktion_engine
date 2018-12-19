f1f321e3067cf3b4bdceba7ba2e603836f7c25a0
Roland Rabien <roland@rabien.com>
2018-12-18 20:33:07 -0800

    AirWindows:  Added plugin types

--- modules/tracktion_engine/plugins/airwindows/tracktion_AirWindows.h
+++ modules/tracktion_engine/plugins/airwindows/tracktion_AirWindows.h
@@ -29,6 +29,20 @@ public:
 class AirWindowsPlugin   : public Plugin
 {
 public:
+    enum Type
+    {
+        delay,
+        distortion,
+        dynamics,
+        emulation,
+        eq,
+        filter,
+        imaging,
+        modulation,
+        reverb,
+        utility,
+    };
+    
     //==============================================================================
     AirWindowsPlugin (PluginCreationInfo, std::unique_ptr<AirWindowsBase>);
     ~AirWindowsPlugin();
@@ -91,68 +105,70 @@ public: \
     \
     static const char* getPluginName()                                      { return NEEDS_TRANS(pluginName); } \
     static const char* xmlTypeName; \
+    \
+    static Type pluginType; \
 }; \
 
 AIRWINDOWSPLUGIN(AirWindowsAcceleration, "Acceleration");
-AIRWINDOWSPLUGIN(AirWindowsADClip7, "ADClip7");
+AIRWINDOWSPLUGIN(AirWindowsADClip7, "AD Clip 7");
 AIRWINDOWSPLUGIN(AirWindowsADT, "ADT");
 AIRWINDOWSPLUGIN(AirWindowsAtmosphere, "Atmosphere");
 AIRWINDOWSPLUGIN(AirWindowsAura, "Aura");
 AIRWINDOWSPLUGIN(AirWindowsBassKit, "BassKit");
-AIRWINDOWSPLUGIN(AirWindowsBitGlitter, "BitGlitter");
-AIRWINDOWSPLUGIN(AirWindowsButterComp, "ButterComp");
-AIRWINDOWSPLUGIN(AirWindowsButterComp2, "ButterComp2");
-AIRWINDOWSPLUGIN(AirWindowsChannel4, "Channel4");
-AIRWINDOWSPLUGIN(AirWindowsChannel5, "Channel5");
-AIRWINDOWSPLUGIN(AirWindowsChorusEnsemble, "ChorusEnsemble");
-AIRWINDOWSPLUGIN(AirWindowsCrunchyGrooveWear, "CrunchyGrooveWear");
+AIRWINDOWSPLUGIN(AirWindowsBitGlitter, "Bit Glitter");
+AIRWINDOWSPLUGIN(AirWindowsButterComp, "Butter Comp");
+AIRWINDOWSPLUGIN(AirWindowsButterComp2, "Butter Comp 2");
+AIRWINDOWSPLUGIN(AirWindowsChannel4, "Channel 4");
+AIRWINDOWSPLUGIN(AirWindowsChannel5, "Channel 5");
+AIRWINDOWSPLUGIN(AirWindowsChorusEnsemble, "Chorus Ensemble");
+AIRWINDOWSPLUGIN(AirWindowsCrunchyGrooveWear, "Crunchy Groove Wear");
 AIRWINDOWSPLUGIN(AirWindowsDeEss, "DeEss");
 AIRWINDOWSPLUGIN(AirWindowsDensity, "Density");
 AIRWINDOWSPLUGIN(AirWindowsDeRez, "DeRez");
 AIRWINDOWSPLUGIN(AirWindowsDesk, "Desk");
-AIRWINDOWSPLUGIN(AirWindowsDesk4, "Desk4");
-AIRWINDOWSPLUGIN(AirWindowsDistance2, "Distance2");
+AIRWINDOWSPLUGIN(AirWindowsDesk4, "Desk 4");
+AIRWINDOWSPLUGIN(AirWindowsDistance2, "Distance 2");
 AIRWINDOWSPLUGIN(AirWindowsDrive, "Drive");
-AIRWINDOWSPLUGIN(AirWindowsDrumSlam, "DrumSlam");
-AIRWINDOWSPLUGIN(AirWindowsDubSub, "DubSub");
-AIRWINDOWSPLUGIN(AirWindowsEdIsDim, "EdIsDim");
-AIRWINDOWSPLUGIN(AirWindowsElectroHat, "ElectroHat");
+AIRWINDOWSPLUGIN(AirWindowsDrumSlam, "Drum Slam");
+AIRWINDOWSPLUGIN(AirWindowsDubSub, "Dub Sub");
+AIRWINDOWSPLUGIN(AirWindowsEdIsDim, "Ed Is Dim");
+AIRWINDOWSPLUGIN(AirWindowsElectroHat, "Electro Hat");
 AIRWINDOWSPLUGIN(AirWindowsEnergy, "Energy");
 AIRWINDOWSPLUGIN(AirWindowsEnsemble, "Ensemble");
-AIRWINDOWSPLUGIN(AirWindowsFathomFive, "FathomFive");
+AIRWINDOWSPLUGIN(AirWindowsFathomFive, "Fathom Five");
 AIRWINDOWSPLUGIN(AirWindowsFloor, "Floor");
-AIRWINDOWSPLUGIN(AirWindowsFromTape, "FromTape");
+AIRWINDOWSPLUGIN(AirWindowsFromTape, "From Tape");
 AIRWINDOWSPLUGIN(AirWindowsGatelope, "Gatelope");
 AIRWINDOWSPLUGIN(AirWindowsGolem, "Golem");
-AIRWINDOWSPLUGIN(AirWindowsGrooveWear, "GrooveWear");
+AIRWINDOWSPLUGIN(AirWindowsGrooveWear, "Groove Wear");
 AIRWINDOWSPLUGIN(AirWindowsGuitarConditioner, "Guitar Conditioner");
 AIRWINDOWSPLUGIN(AirWindowsHardVacuum, "Hard Vacuum");
 AIRWINDOWSPLUGIN(AirWindowsHombre, "Hombre");
 AIRWINDOWSPLUGIN(AirWindowsMelt, "Melt");
 AIRWINDOWSPLUGIN(AirWindowsNC17, "NC-17");
 AIRWINDOWSPLUGIN(AirWindowsNoise, "Noise");
-AIRWINDOWSPLUGIN(AirWindowsNonlinearSpace, "NonlinearSpace");
+AIRWINDOWSPLUGIN(AirWindowsNonlinearSpace, "Nonlinear Space");
 AIRWINDOWSPLUGIN(AirWindowsPoint, "Point");
 AIRWINDOWSPLUGIN(AirWindowsPop, "Pop");
-AIRWINDOWSPLUGIN(AirWindowsPressure4, "Pressure4");
-AIRWINDOWSPLUGIN(AirWindowsPurestDrive, "PurestDrive");
-AIRWINDOWSPLUGIN(AirWindowsPurestWarm, "PurestWarm");
-AIRWINDOWSPLUGIN(AirWindowsRighteous4, "Righteous4");
-AIRWINDOWSPLUGIN(AirWindowsSingleEndedTriode, "SingleEndedTriode");
-AIRWINDOWSPLUGIN(AirWindowsSlewOnly, "SlewOnly");
-AIRWINDOWSPLUGIN(AirWindowsSpiral2, "Spiral2");
-AIRWINDOWSPLUGIN(AirWindowsStarChild, "StarChild");
-AIRWINDOWSPLUGIN(AirWindowsStereoFX, "StereoFX");
-AIRWINDOWSPLUGIN(AirWindowsSubsOnly, "SubsOnly");
+AIRWINDOWSPLUGIN(AirWindowsPressure4, "Pressure 4");
+AIRWINDOWSPLUGIN(AirWindowsPurestDrive, "Purest Drive");
+AIRWINDOWSPLUGIN(AirWindowsPurestWarm, "Purest Warm");
+AIRWINDOWSPLUGIN(AirWindowsRighteous4, "Righteous 4");
+AIRWINDOWSPLUGIN(AirWindowsSingleEndedTriode, "Single Ended Triode");
+AIRWINDOWSPLUGIN(AirWindowsSlewOnly, "Slew Only");
+AIRWINDOWSPLUGIN(AirWindowsSpiral2, "Spiral 2");
+AIRWINDOWSPLUGIN(AirWindowsStarChild, "Star Child");
+AIRWINDOWSPLUGIN(AirWindowsStereoFX, "Stereo FX");
+AIRWINDOWSPLUGIN(AirWindowsSubsOnly, "Subs Only");
 AIRWINDOWSPLUGIN(AirWindowsSurge, "Surge");
 AIRWINDOWSPLUGIN(AirWindowsSwell, "Swell");
-AIRWINDOWSPLUGIN(AirWindowsTapeDust, "TapeDust");
+AIRWINDOWSPLUGIN(AirWindowsTapeDust, "Tape Dust");
 AIRWINDOWSPLUGIN(AirWindowsThunder, "Thunder");
 AIRWINDOWSPLUGIN(AirWindowsToTape5, "ToTape 5");
-AIRWINDOWSPLUGIN(AirWindowsToVinyl4, "ToVinyl4");
-AIRWINDOWSPLUGIN(AirWindowsTubeDesk, "TubeDesk");
+AIRWINDOWSPLUGIN(AirWindowsToVinyl4, "To Vinyl 4");
+AIRWINDOWSPLUGIN(AirWindowsTubeDesk, "Tube Desk");
 AIRWINDOWSPLUGIN(AirWindowsUnbox, "Unbox");
-AIRWINDOWSPLUGIN(AirWindowsVariMu, "VariMu");
+AIRWINDOWSPLUGIN(AirWindowsVariMu, "Vari Mu");
 AIRWINDOWSPLUGIN(AirWindowsVoiceOfTheStarship, "Voice Of The Starship");
 AIRWINDOWSPLUGIN(AirWindowsWider, "Wider");
 
