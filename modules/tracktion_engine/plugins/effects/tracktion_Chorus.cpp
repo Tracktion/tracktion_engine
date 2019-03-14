b31ee5e54187912ebb8b85318086598eed0706ae
Roland Rabien <roland@rabien.com>
2019-03-14 11:29:41 -0700

    Chorus:   Fixed rate geting less accurate as time passes

--- modules/tracktion_engine/plugins/effects/tracktion_Chorus.cpp
+++ modules/tracktion_engine/plugins/effects/tracktion_Chorus.cpp
@@ -113,6 +113,9 @@ void ChorusPlugin::applyToBuffer (const AudioRenderContext& fc)
     zeroDenormalisedValuesIfNeeded (*fc.destBuffer);
 
     phase = ph;
+    if (phase >= MathConstants<float>::pi)
+        phase -= MathConstants<float>::pi;
+    
     delayBuffer.bufferPos = bufPos;
 }
 
