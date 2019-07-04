8e53783b03f723c608274b31e01549ee5a2087b8
Roland Rabien <roland@rabien.com>
2019-07-04 11:56:16 -0700

    Fix sidechain source listeners geting called before CachedValue updated

--- modules/tracktion_engine/plugins/tracktion_Plugin.cpp
+++ modules/tracktion_engine/plugins/tracktion_Plugin.cpp
@@ -539,8 +539,14 @@ void Plugin::setSidechainSourceByName (const String& name)
         }
     }
 
-    if (! found)
-        sidechainSourceID.resetToDefault();
+	if (!found)
+	{
+		// juce does not call listeners correctly when using 
+		// resetToDefault. So first set it to default value
+		// manually and then remove it from value tree.
+		sidechainSourceID = sidechainSourceID.getDefault();
+		sidechainSourceID.resetToDefault();
+	}
 }
 
 void Plugin::guessSidechainRouting()
