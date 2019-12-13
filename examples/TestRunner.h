d19f3fc703b6afa46d6294a7cb542b94d7608fff
David Rowland <drowland96@gmail.com>
2019-12-13 11:36:00 +0000

    Recording Latency:   Refactored the test structure

--- examples/TestRunner.h
+++ examples/TestRunner.h
@@ -29,47 +29,6 @@
 
 #include <thread>
 
-template<class Clock, class Duration>
-void yield_until (const std::chrono::time_point<Clock, Duration>& sleep_time)
-{
-    while (Clock::now() < sleep_time)
-        std::this_thread::yield();
-}
-
-template <class Rep, class Period>
-void yield_for (const std::chrono::duration<Rep, Period>& sleep_duration)
-{
-    yield_until (std::chrono::steady_clock::now() + sleep_duration);
-}
-
-int64_t findImpulseSampleIndex (File& file)
-{
-    if (auto reader = std::unique_ptr<AudioFormatReader> (tracktion_engine::AudioFileUtils::createReaderFor (file)))
-        return reader->searchForLevel (0, reader->lengthInSamples,
-                                       0.9f, 1.1f,
-                                       0);
-
-    return -1;
-}
-
-struct TempCurrentWorkingDirectory
-{
-    TempCurrentWorkingDirectory()
-    {
-        tempDir.createDirectory();
-        tempDir.setAsCurrentWorkingDirectory();
-    }
-
-    ~TempCurrentWorkingDirectory()
-    {
-        tempDir.deleteRecursively (false);
-        originalCwd.setAsCurrentWorkingDirectory();
-    }
-
-private:
-    const File originalCwd = File::getCurrentWorkingDirectory();
-    const File tempDir = File::createTempFile ({});
-};
 
 namespace tracktion_engine
 {
@@ -85,17 +44,29 @@ public:
     //==============================================================================
     void runTest() override
     {
+        HostedAudioDeviceInterface::Parameters params;
+        params.sampleRate = 44100.0;
+        params.blockSize = 256;
+        params.inputChannels = 16;
+        params.fixedBlockSize = true;
+        runSyncronisationTest (params);
+
+        params.sampleRate = 96000.0;
+        params.blockSize = 512;
+        runSyncronisationTest (params);
+
+        params.blockSize = 64;
+        runSyncronisationTest (params);
+    }
+
+    void runSyncronisationTest (const HostedAudioDeviceInterface::Parameters& params)
+    {
         using namespace std::chrono_literals;
 
         Engine& engine = Engine::getInstance();
         auto& deviceManager = engine.getDeviceManager();
         auto& audioIO = deviceManager.getHostedAudioDeviceInterface();
 
-        HostedAudioDeviceInterface::Parameters params;
-        params.sampleRate = 44100.0;
-        params.blockSize = 256;
-        params.inputChannels = 16;
-        params.fixedBlockSize = true;
         audioIO.initialise (params);
         audioIO.prepareToPlay (params.sampleRate, params.blockSize);
 
@@ -106,7 +77,56 @@ public:
         }
 
         TempCurrentWorkingDirectory tempDir;
+        auto edit = createEditWithTracksForInputs (engine, params);
+        auto& transport = edit->getTransport();
 
+        // Start recording, add an impulse after 1s then wait another 1s and stop recording
+        {
+            ProcessThread processThread (audioIO, params);
+
+            transport.stop (false, false);
+
+            transport.record (false, false);
+            std::this_thread::sleep_for (1s);
+
+            processThread.insertImpulseIntoNextBlock();
+            std::this_thread::sleep_for (1s);
+
+            transport.stop (false, true);
+        }
+
+        beginTest ("Test injected impulses align");
+        {
+            // Get recorded audio files and check the impulse is in the same place
+            auto clips = getAllClipsFromTracks<WaveAudioClip> (*edit);
+            expectEquals (clips.size(), getAudioTracks (*edit).size());
+            auto audioFiles = getSourceFilesFromClips (clips);
+            auto sampleIndicies = getSampleIndiciesOfImpulse (audioFiles);
+
+            expectEquals ((int) std::count_if (sampleIndicies.begin(), sampleIndicies.end(),
+                                               [] (auto index) { return index != - 1; }),
+                          audioFiles.size(), "Some files don't have an impulse in them");
+
+            for (auto index : sampleIndicies)
+                if (index != sampleIndicies[0])
+                    expect (false, String ("Mismatch of impulse indicies (FIRST & SECOND samples, difference of DIFF)")
+                            .replace ("FIRST", String (index))
+                            .replace ("SECOND", String (sampleIndicies[0]))
+                            .replace ("DIFF", String (index - sampleIndicies[0])));
+        }
+    }
+
+    //==============================================================================
+    template<class Clock, class Duration>
+    static void yield_until (const std::chrono::time_point<Clock, Duration>& sleep_time)
+    {
+        while (Clock::now() < sleep_time)
+            std::this_thread::yield();
+    }
+
+    //==============================================================================
+    std::unique_ptr<Edit> createEditWithTracksForInputs (Engine& engine, const HostedAudioDeviceInterface::Parameters& params)
+    {
         auto edit = std::make_unique<Edit> (Edit::Options { engine, createEmptyEdit(), ProjectItemID::createNewID (0) });
         auto& transport = edit->getTransport();
         transport.ensureContextAllocated();
@@ -127,70 +147,123 @@ public:
             inputInstance->setRecordingEnabled (true);
         }
 
-        AudioBuffer<float> silenceBuffer (params.inputChannels, params.blockSize);
-        silenceBuffer.clear();
+        return edit;
+    }
+
+    //==============================================================================
+    struct ProcessThread
+    {
+        ProcessThread (HostedAudioDeviceInterface& interface, const HostedAudioDeviceInterface::Parameters& params)
+            : audioIO (interface)
+        {
+            silenceBuffer.setSize (params.inputChannels, params.blockSize);
+            silenceBuffer.clear();
+            const int msPerBlock = roundToInt ((params.blockSize / params.sampleRate) * 1000.0);
+
+            processThread = std::thread ([&, msPerBlock]
+                                         {
+                                             while (! shouldStop.load())
+                                             {
+                                                 auto endTime = std::chrono::steady_clock::now() + std::chrono::milliseconds (msPerBlock);
+
+                                                 if (insertImpulse.exchange (false))
+                                                     for (int c = silenceBuffer.getNumChannels(); --c >= 0;)
+                                                         silenceBuffer.setSample (c, 0, 1.0f);
+
+                                                 audioIO.processBlock (silenceBuffer, emptyMidiBuffer);
+                                                 silenceBuffer.clear();
+                                                 emptyMidiBuffer.clear();
+
+                                                 yield_until (endTime);
+                                             }
+                                         });
+        }
+
+        ~ProcessThread()
+        {
+            shouldStop.store (true);
+            processThread.join();
+        }
+
+        void insertImpulseIntoNextBlock()
+        {
+            insertImpulse.store (true);
+        }
+
+    private:
+        HostedAudioDeviceInterface& audioIO;
+        HostedAudioDeviceInterface::Parameters parameters;
+
+        AudioBuffer<float> silenceBuffer;
         MidiBuffer emptyMidiBuffer;
-        const int msPerBlock = roundToInt ((params.blockSize / params.sampleRate) * 1000.0);
+
+        std::thread processThread;
         std::atomic<bool> shouldStop { false }, insertImpulse { false };
+    };
 
-        std::thread processThread ([&]
-                                   {
-                                       while (! shouldStop.load())
-                                       {
-                                           auto endTime = std::chrono::steady_clock::now() + std::chrono::milliseconds (msPerBlock);
+    //==============================================================================
+    struct TempCurrentWorkingDirectory
+    {
+        TempCurrentWorkingDirectory()
+        {
+            tempDir.createDirectory();
+            tempDir.setAsCurrentWorkingDirectory();
+        }
 
-                                           if (insertImpulse.exchange (false))
-                                               for (int c = silenceBuffer.getNumChannels(); --c >= 0;)
-                                                   silenceBuffer.setSample (c, 0, 1.0f);
+        ~TempCurrentWorkingDirectory()
+        {
+            tempDir.deleteRecursively (false);
+            originalCwd.setAsCurrentWorkingDirectory();
+        }
 
-                                           audioIO.processBlock (silenceBuffer, emptyMidiBuffer);
-                                           silenceBuffer.clear();
+    private:
+        const File originalCwd = File::getCurrentWorkingDirectory();
+        const File tempDir = File::createTempFile ({});
+    };
 
-                                           yield_until (endTime);
-                                       }
-                                   });
+    //==============================================================================
+    template<typename ClipType>
+    Array<ClipType*> getAllClipsFromTracks (Edit& edit)
+    {
+        Array<ClipType*> clips;
 
-        // Start recording, add an impulse after 1s then wait another 1s and stop recording
-        transport.stop (false, false);
-        transport.record (false, false);
-        yield_for (1s);
-        insertImpulse.store (true);
-        yield_for (1s);
-        transport.stop (false, true);
-
-        shouldStop.store (true);
-        processThread.join();
-
-        // Get recorded audio files and check the impulse is in the same place
-        Array<WaveAudioClip*> clips;
-
-        for (auto audioTrack : audioTracks)
+        for (auto audioTrack : getAudioTracks (edit))
             for (auto clip : audioTrack->getClips())
-                if (auto waveClip = dynamic_cast<WaveAudioClip*> (clip))
+                if (auto waveClip = dynamic_cast<ClipType*> (clip))
                     clips.add (waveClip);
 
-        expectEquals (clips.size(), audioTracks.size());
+        return clips;
+    }
 
-        Array<File> audioFiles;
+    template<typename ClipType>
+    Array<File> getSourceFilesFromClips (const Array<ClipType*>& clips)
+    {
+        Array<File> files;
 
         for (auto clip : clips)
-            audioFiles.add (clip->getCurrentSourceFile());
+            files.add (clip->getCurrentSourceFile());
 
-        // Find indicies of recorded samples
+        return files;
+    }
+
+    int64_t findImpulseSampleIndex (File& file)
+    {
+        if (auto reader = std::unique_ptr<AudioFormatReader> (tracktion_engine::AudioFileUtils::createReaderFor (file)))
+            return reader->searchForLevel (0, reader->lengthInSamples,
+                                           0.9f, 1.1f,
+                                           0);
+
+        return -1;
+    }
+
+    std::vector<int64_t> getSampleIndiciesOfImpulse (const Array<File>& files)
+    {
         std::vector<int64_t> sampleIndicies;
 
-        for (auto audioFile : audioFiles)
-            sampleIndicies.push_back (findImpulseSampleIndex (audioFile));
+        for (auto file : files)
+            sampleIndicies.push_back (findImpulseSampleIndex (file));
 
-        expectEquals ((int) std::count_if (sampleIndicies.begin(), sampleIndicies.end(), [] (auto index) { return index != -1; }),
-                      audioFiles.size(), "Some files don't have an impulse in them");
-
-        for (auto index : sampleIndicies)
-            if (index != sampleIndicies[0])
-                expect (false, String ("Mismatch of impulse indicies (FIRST & SECOND samples, difference of DIFF)")
-                                   .replace ("FIRST", String (index))
-                                   .replace ("SECOND", String (sampleIndicies[0]))
-                                   .replace ("DIFF", String (index - sampleIndicies[0])));
+        return sampleIndicies;
     }
 };
 
