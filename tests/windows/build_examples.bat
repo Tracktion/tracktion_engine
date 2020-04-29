d29cbcce4b9811b5bdba594d8c663c256a09b6df
David Rowland <drowland96@gmail.com>
2020-04-29 16:13:47 +0100

    Audio Graph:   Fixed clean platform in the Windows build script

--- tests/windows/build_examples.bat
+++ tests/windows/build_examples.bat
@@ -70,7 +70,7 @@ exit /b 0
     if defined DISABLE_BUILD goto builtSection
         cd "%EXAMPLE_ROOT_DIR%/Builds/VisualStudio2017"
         set CL=/DJUCER_ENABLE_GPL_MODE /GL
-        "%MSBUILD_EXE%" %EXAMPLE_NAME%.sln /p:VisualStudioVersion=15.0 /m /t:Clean
+        "%MSBUILD_EXE%" %EXAMPLE_NAME%.sln /p:VisualStudioVersion=15.0 /m /t:Clean /p:Configuration=Release /p:Platform=x64
         "%MSBUILD_EXE%" %EXAMPLE_NAME%.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 /p:TreatWarningsAsErrors="true" /warnaserror
         if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
     :builtSection
