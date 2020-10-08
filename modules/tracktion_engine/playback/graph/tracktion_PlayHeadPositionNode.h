374a362ee0f3d5f3400645f43bef91725b48f667
David Rowland <drowland96@gmail.com>
2020-10-08 13:11:19 +0100

    tracktion_graph:   Fixed an issue where the playhead could jump back if the graph is rebuilt before process has been called

--- modules/tracktion_engine/playback/graph/tracktion_PlayHeadPositionNode.h
+++ modules/tracktion_engine/playback/graph/tracktion_PlayHeadPositionNode.h
@@ -28,7 +28,14 @@ public:
     
     tracktion_graph::NodeProperties getNodeProperties() override
     {
-        return input->getNodeProperties();
+        auto props = input->getNodeProperties();
+        
+        constexpr size_t playHeadPositionNodeMagicHash = 0x706c617948656164;
+        
+        if (props.nodeID != 0)
+            tracktion_graph::hash_combine (props.nodeID, playHeadPositionNodeMagicHash);
+
+        return props;
     }
     
     std::vector<tracktion_graph::Node*> getDirectInputNodes() override  { return { input.get() }; }
@@ -37,6 +44,10 @@ public:
     void prepareToPlay (const tracktion_graph::PlaybackInitialisationInfo& info) override
     {
         latencyNumSamples = info.rootNode.getNodeProperties().latencyNumSamples;
+        
+        // Member variables have to be updated from the previous Node or if the graph gets
+        // rebuilt during the countdown period, the playhead time will jump back
+        updateFromPreviousNode (info.rootNodeToReplace);
     }
     
     void process (const ProcessContext& pc) override
@@ -56,6 +67,7 @@ private:
     std::atomic<double>& playHeadTime;
 
     int latencyNumSamples = 0;
+    bool updateReferencePositionOnJump = true;
     
     int64_t numLatencySamplesToCountDown = 0;
     int64_t lastReferenceSamplePosition = 0;
@@ -65,10 +77,11 @@ private:
     {
         int64_t referenceSamplePosition = getReferenceSampleRange().getStart();
         
-        if (getPlayHeadState().didPlayheadJump())
+        if (getPlayHeadState().didPlayheadJump() || updateReferencePositionOnJump)
         {
             numLatencySamplesToCountDown = latencyNumSamples;
-            referencePositionOnJump = lastReferenceSamplePosition;
+            referencePositionOnJump = updateReferencePositionOnJump ? referenceSamplePosition - numSamples
+                                                                    : lastReferenceSamplePosition;
             // TODO: Need to determine why the last position is the "correct" visual position
         }
         
@@ -93,6 +106,33 @@ private:
         const double time = tracktion_graph::sampleToTime (timelinePosition, getSampleRate());
 
         playHeadTime = time;
+        updateReferencePositionOnJump = false;
+    }
+
+    void updateFromPreviousNode (Node* rootNodeToReplace)
+    {
+        if (rootNodeToReplace == nullptr)
+            return;
+        
+        auto nodeIDToLookFor = getNodeProperties().nodeID;
+        
+        if (nodeIDToLookFor == 0)
+            return;
+
+        auto visitor = [this, nodeIDToLookFor] (Node& node)
+        {
+            if (auto other = dynamic_cast<PlayHeadPositionNode*> (&node))
+            {
+                if (other->getNodeProperties().nodeID == nodeIDToLookFor)
+                {
+                    numLatencySamplesToCountDown = other->numLatencySamplesToCountDown;
+                    lastReferenceSamplePosition = other->lastReferenceSamplePosition;
+                    referencePositionOnJump = other->referencePositionOnJump;
+                    updateReferencePositionOnJump = false;
+                }
+            }
+        };
+        visitNodes (*rootNodeToReplace, visitor, true);
     }
 };
 
