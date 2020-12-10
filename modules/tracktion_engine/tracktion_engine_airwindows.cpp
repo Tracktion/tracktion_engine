ba920f5e03fe8c89942bb8583091b1505e3ebe4a
Joël R. Langlois <joel.r.langlois@gmail.com>
2020-12-10 11:39:32 -0500

    Fixed compilation when enabling AirWindows with MSVC.

--- modules/tracktion_engine/tracktion_engine_airwindows.cpp
+++ modules/tracktion_engine/tracktion_engine_airwindows.cpp
@@ -9,6 +9,9 @@
 
 #if ! JUCE_PROJUCER_LIVE_BUILD
 
+#undef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES 1
+
 #include <atomic>
 #include <numeric>
 #include <set>
@@ -50,7 +53,7 @@ using namespace juce;
 
 #if JUCE_WINDOWS
  #pragma warning (push)
- #pragma warning (disable : 4244 4100 4305 4065 4701 4706 4723)
+ #pragma warning (disable : 4244 4100 4305 4065 4701 4706 4723 4458)
 #endif
 
 namespace tracktion_engine
