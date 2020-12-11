f6d76522910574e1b634ed2a5dc9c7946b165dac
David Rowland <drowland96@gmail.com>
2020-12-11 17:38:39 +0000

    Updated minimum library requirements to C++17; Added CMake builds to examples; Please now use the single "tests/build" file to generate example projects and run tests etc.

---
+++ examples/TestRunner/TestRunner.cpp
@@ -0,0 +1,12 @@
+/*
+    ,--.                     ,--.     ,--.  ,--.
+  ,-'  '-.,--.--.,--,--.,---.|  |,-.,-'  '-.`--' ,---. ,--,--,      Copyright 2018
+  '-.  .-'|  .--' ,-.  | .--'|     /'-.  .-',--.| .-. ||      \   Tracktion Software
+    |  |  |  |  \ '-'  \ `--.|  \  \  |  |  |  |' '-' '|  ||  |       Corporation
+    `---' `--'   `--`--'`---'`--'`--' `---' `--' `---' `--''--'    www.tracktion.com
+
+    Tracktion Engine uses a GPL/commercial licence - see LICENCE.md for details.
+*/
+
+#include <JuceHeader.h>
+#include "../TestRunner.h"
