aaa01abb2a86adeff3d9c5d17510e30694c19a4a
Joël R. Langlois <joel.r.langlois@gmail.com>
2021-01-04 10:02:21 -0500

    EditTimeRange: Added constexpr and noexcept where possible, and added a couple more methods.

--- modules/tracktion_engine/model/tracks/tracktion_EditTimeRange.cpp
+++ modules/tracktion_engine/model/tracks/tracktion_EditTimeRange.cpp
@@ -11,108 +11,103 @@
 namespace tracktion_engine
 {
 
-EditTimeRange::EditTimeRange (double s, double e) : start (s), end (e)
+EditTimeRange::EditTimeRange (double s, double e) noexcept
+    : start (s), end (e)
 {
     jassert (s <= e);
 }
 
-EditTimeRange::EditTimeRange (juce::Range<double> timeRange)
+EditTimeRange::EditTimeRange (juce::Range<double> timeRange) noexcept
     : EditTimeRange (timeRange.getStart(), timeRange.getEnd())
 {
 }
 
-EditTimeRange EditTimeRange::between (double time1, double time2)
+EditTimeRange EditTimeRange::between (double time1, double time2) noexcept
 {
-    return time1 <= time2 ? EditTimeRange (time1, time2)
-                          : EditTimeRange (time2, time1);
+    return { std::min (time1, time2), std::max (time1, time2) };
 }
 
-EditTimeRange EditTimeRange::withStartAndLength (double startValue, double length)
+EditTimeRange EditTimeRange::withStartAndLength (double startValue, double length) noexcept
 {
     jassert (length >= 0.0);
     return Range<double>::withStartAndLength (startValue, length);
 }
 
-EditTimeRange EditTimeRange::emptyRange (double start)
+EditTimeRange EditTimeRange::emptyRange (double start) noexcept
 {
-    return EditTimeRange (start, start);
+    return { start, start };
 }
 
-EditTimeRange EditTimeRange::getUnionWith (EditTimeRange other) const
+EditTimeRange EditTimeRange::getUnionWith (const EditTimeRange& other) const noexcept
 {
     return { std::min (start, other.start),
              std::max (end, other.end) };
 }
 
-EditTimeRange EditTimeRange::getIntersectionWith (EditTimeRange other) const
+EditTimeRange EditTimeRange::getIntersectionWith (const EditTimeRange& other) const noexcept
 {
-    auto newStart = std::max (start, other.start);
+    const auto newStart = std::max (start, other.start);
     return { newStart, std::max (newStart, std::min (end, other.end)) };
 }
 
-EditTimeRange EditTimeRange::constrainRange (EditTimeRange rangeToConstrain) const
+EditTimeRange EditTimeRange::constrainRange (const EditTimeRange& rangeToConstrain) const noexcept
 {
-    auto otherLen = rangeToConstrain.getLength();
+    const auto otherLen = rangeToConstrain.getLength();
 
     return getLength() <= otherLen
             ? *this
             : rangeToConstrain.movedToStartAt (jlimit (start, end - otherLen, rangeToConstrain.getStart()));
 }
 
-EditTimeRange EditTimeRange::rescaled (double anchorTime, double factor) const
+EditTimeRange EditTimeRange::rescaled (double anchorTime, double factor) const noexcept
 {
     jassert (factor > 0);
     return { anchorTime + (start - anchorTime) * factor,
              anchorTime + (end   - anchorTime) * factor };
 }
 
-EditTimeRange EditTimeRange::expanded (double amount) const
+EditTimeRange EditTimeRange::expanded (double amount) const noexcept
 {
     jassert (amount >= 0);
     return { start - amount, end + amount };
 }
 
-EditTimeRange EditTimeRange::reduced (double amount) const
+EditTimeRange EditTimeRange::reduced (double amount) const noexcept
 {
     jassert (amount >= 0);
     amount = std::min (amount, getLength() / 2.0);
     return { start + amount, end - amount };
 }
 
-EditTimeRange EditTimeRange::movedToStartAt (double newStart) const
+EditTimeRange EditTimeRange::movedToStartAt (double newStart) const noexcept
 {
     return { newStart, end + (newStart - start) };
 }
 
-EditTimeRange EditTimeRange::movedToEndAt (double newEnd) const
+EditTimeRange EditTimeRange::movedToEndAt (double newEnd) const noexcept
 {
     return { start + (newEnd - end), newEnd };
 }
 
-EditTimeRange EditTimeRange::withStart (double newStart) const
+EditTimeRange EditTimeRange::withStart (double newStart) const noexcept
 {
     jassert (newStart <= end);
     return { newStart, std::max (end, newStart) };
 }
 
-EditTimeRange EditTimeRange::withEnd (double newEnd) const
+EditTimeRange EditTimeRange::withEnd (double newEnd) const noexcept
 {
     jassert (newEnd >= start);
     return { std::min (start, newEnd), newEnd };
 }
 
-EditTimeRange EditTimeRange::withLength (double newLength) const
+EditTimeRange EditTimeRange::withLength (double newLength) const noexcept
 {
     jassert (newLength >= 0);
     return { start, start + std::max (0.0, newLength) };
 }
 
-EditTimeRange EditTimeRange::operator+ (double amount) const
-{
-    return { start + amount, end + amount };
-}
-
-ClipPosition ClipPosition::rescaled (double anchorTime, double factor) const
+ClipPosition ClipPosition::rescaled (double anchorTime, double factor) const noexcept
 {
     return { time.rescaled (anchorTime, factor), offset * factor };
 }
--- modules/tracktion_engine/model/tracks/tracktion_EditTimeRange.h
+++ modules/tracktion_engine/model/tracks/tracktion_EditTimeRange.h
@@ -15,55 +15,111 @@ namespace tracktion_engine
 */
 struct EditTimeRange
 {
-    EditTimeRange() = default;
-    EditTimeRange (const EditTimeRange&) = default;
-    EditTimeRange& operator= (const EditTimeRange&) = default;
+    /** */
+    EditTimeRange() noexcept = default;
+    /** */
+    constexpr EditTimeRange (const EditTimeRange&) noexcept = default;
+    /** */
+    EditTimeRange& operator= (const EditTimeRange&) noexcept = default;
+    /** */
+    ~EditTimeRange() noexcept = default;
 
-    EditTimeRange (double start, double end);
-    EditTimeRange (juce::Range<double> timeRange);
+    /** */
+    EditTimeRange (double start, double end) noexcept;
+    /** */
+    EditTimeRange (juce::Range<double> timeRange) noexcept;
 
+    //==============================================================================
     /** Returns the range that lies between two positions (in either order). */
-    static EditTimeRange between (double time1, double time2);
+    static EditTimeRange between (double time1, double time2) noexcept;
 
     /** Returns a range with a given start and length. */
-    static EditTimeRange withStartAndLength (double time1, double length);
+    static EditTimeRange withStartAndLength (double time1, double length) noexcept;
 
     /** Returns a range with the specified start position and a length of zero. */
-    static EditTimeRange emptyRange (double start);
+    static EditTimeRange emptyRange (double start) noexcept;
 
-    double start = 0;
-    double end = 0;
+    //==============================================================================
+    /** */
+    constexpr double getStart() const noexcept                  { return start; }
+    /** */
+    constexpr double getEnd() const noexcept                    { return end; }
+    /** */
+    constexpr double getLength() const noexcept                 { return end - start; }
+    /** */
+    constexpr double getCentre() const noexcept                 { return (start + end) / 2.0; }
+    /** */
+    double clipValue (double value) const noexcept              { return juce::jlimit (start, end, value); }
 
-    double getStart() const                 { return start; }
-    double getEnd() const                   { return end; }
-    double getLength() const                { return end - start; }
-    double getCentre() const                { return (start + end) * 0.5; }
-    double clipValue (double value) const   { return juce::jlimit (start, end, value); }
+    /** */
+    constexpr bool isEmpty() const noexcept                     { return end <= start; }
 
-    bool isEmpty() const                    { return end <= start; }
+    /** */
+    constexpr juce::Range<double> toRange() const noexcept      { return { start, end }; }
 
-    bool operator== (const EditTimeRange& other) const  { return start == other.start && end == other.end; }
-    bool operator!= (const EditTimeRange& other) const  { return ! operator== (other); }
+    //==============================================================================
+    /** */
+    constexpr bool overlaps (const EditTimeRange& other) const noexcept { return other.start < end && start < other.end; }
+    /** */
+    constexpr bool contains (const EditTimeRange& other) const noexcept { return other.start >= start && other.end <= end; }
+    /** */
+    constexpr bool contains (double time) const noexcept                { return time >= start && time < end; }
+    /** */
+    constexpr bool containsInclusive (double time) const noexcept       { return time >= start && time <= end; }
 
-    bool overlaps (const EditTimeRange& other) const    { return other.start < end && start < other.end; }
-    bool contains (const EditTimeRange& other) const    { return other.start >= start && other.end <= end; }
-    bool contains (double time) const                   { return time >= start && time < end; }
-    bool containsInclusive (double time) const          { return time >= start && time <= end; }
+    //==============================================================================
+    /** */
+    EditTimeRange getUnionWith (const EditTimeRange& other) const noexcept;
+    /** */
+    EditTimeRange getIntersectionWith (const EditTimeRange& other) const noexcept;
+    /** */
+    EditTimeRange rescaled (double anchorTime, double factor) const noexcept;
+    /** */
+    EditTimeRange constrainRange (const EditTimeRange& rangeToConstrain) const noexcept;
+    /** */
+    EditTimeRange expanded (double amount) const noexcept;
+    /** */
+    EditTimeRange reduced (double amount) const noexcept;
+    /** */
+    EditTimeRange movedToStartAt (double newStart) const noexcept;
+    /** */
+    EditTimeRange movedToEndAt (double newEnd) const noexcept;
+    /** */
+    EditTimeRange withStart (double newStart) const noexcept;
+    /** */
+    EditTimeRange withEnd (double newEnd) const noexcept;
+    /** */
+    EditTimeRange withLength (double newLength) const noexcept;
 
-    EditTimeRange getUnionWith (EditTimeRange other) const;
-    EditTimeRange getIntersectionWith (EditTimeRange other) const;
-    EditTimeRange rescaled (double anchorTime, double factor) const;
-    EditTimeRange constrainRange (EditTimeRange rangeToConstrain) const;
-    EditTimeRange expanded (double amount) const;
-    EditTimeRange reduced (double amount) const;
-    EditTimeRange movedToStartAt (double newStart) const;
-    EditTimeRange movedToEndAt (double newEnd) const;
-    EditTimeRange withStart (double newStart) const;
-    EditTimeRange withEnd (double newEnd) const;
-    EditTimeRange withLength (double newLength) const;
+    //==============================================================================
+    /** */
+    constexpr bool operator== (const EditTimeRange& other) const noexcept   { return start == other.start && end == other.end; }
+    /** */
+    constexpr bool operator!= (const EditTimeRange& other) const noexcept   { return ! operator== (other); }
 
-    EditTimeRange operator+ (double amount) const;
-    EditTimeRange operator- (double amount) const       { return operator+ (-amount); }
+    /** */
+    EditTimeRange operator+ (double amount) const noexcept                  { return { start + amount, end + amount }; }
+    /** */
+    EditTimeRange operator- (double amount) const noexcept                  { return operator+ (-amount); }
+
+    /** */
+    EditTimeRange operator+= (double amount) noexcept 
+    {
+        start += amount;
+        end += amount;
+        return *this;
+    }
+
+    /** */
+    EditTimeRange operator-= (double amount) noexcept
+    {
+        start -= amount;
+        end -= amount;
+        return *this;
+    }
+
+    //==============================================================================
+    double start = 0.0, end = 0.0;
 };
 
 //==============================================================================
@@ -71,19 +127,40 @@ struct EditTimeRange
 */
 struct ClipPosition
 {
+    /** */
+    ClipPosition() noexcept = default;
+    /** */
+    constexpr ClipPosition (const ClipPosition&) noexcept = default;
+    /** */
+    ClipPosition& operator= (const ClipPosition&) noexcept = default;
+    /** */
+    ~ClipPosition() noexcept = default;
+
+    //==============================================================================
+    /** */
+    constexpr double getStart() const noexcept                              { return time.start; }
+    /** */
+    constexpr double getEnd() const noexcept                                { return time.end; }
+    /** */
+    constexpr double getLength() const noexcept                             { return time.getLength(); }
+    /** */
+    constexpr double getOffset() const noexcept                             { return offset; }
+    /** */
+    constexpr double getStartOfSource() const noexcept                      { return time.start - offset; }
+
+    //==============================================================================
+    /** */
+    ClipPosition rescaled (double anchorTime, double factor) const noexcept;
+
+    //==============================================================================
+    /** */
+    constexpr bool operator== (const ClipPosition& other) const noexcept    { return offset == other.offset && time == other.time; }
+    /** */
+    constexpr bool operator!= (const ClipPosition& other) const noexcept    { return ! operator== (other); }
+
+    //==============================================================================
     EditTimeRange time;
-    double offset = 0;
-
-    double getStart() const             { return time.start; }
-    double getEnd() const               { return time.end; }
-    double getLength() const            { return time.getLength(); }
-    double getOffset() const            { return offset; }
-    double getStartOfSource() const     { return time.start - offset; }
-
-    bool operator== (const ClipPosition& other) const  { return offset == other.offset && time == other.time; }
-    bool operator!= (const ClipPosition& other) const  { return ! operator== (other); }
-
-    ClipPosition rescaled (double anchorTime, double factor) const;
+    double offset = 0.0;
 };
 
 } // namespace tracktion_engine
