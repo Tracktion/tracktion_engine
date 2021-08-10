58a4a02fb717834c6866023949cffba3c90e0f01
David Rowland <drowland96@gmail.com>
2021-08-10 11:22:02 +0100

    Added JUCE_MODAL_LOOPS_PERMITTED=1 flag to examples

--- examples/PluginDemo.h
+++ examples/PluginDemo.h
@@ -15,7 +15,7 @@
                     juce_gui_basics, juce_gui_extra, juce_osc, tracktion_engine
   exporters:        linux_make, vs2017, xcode_iphone, xcode_mac
 
-  moduleFlags:      JUCE_STRICT_REFCOUNTEDPOINTER=1, JUCE_PLUGINHOST_AU=1, JUCE_PLUGINHOST_VST3=1
+  moduleFlags:      JUCE_STRICT_REFCOUNTEDPOINTER=1, JUCE_PLUGINHOST_AU=1, JUCE_PLUGINHOST_VST3=1, JUCE_MODAL_LOOPS_PERMITTED=1
 
   type:             Component
   mainClass:        PluginDemo
