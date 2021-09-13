63b2f5d6d735d4cf89081a1e1971f970b14a6d70
David Rowland <drowland96@gmail.com>
2021-09-13 13:09:15 +0100

    Reverted accidental changes to PluginDemo.h

--- examples/PluginDemo.h
+++ examples/PluginDemo.h
@@ -1,20 +1,27 @@
 /*******************************************************************************
-The block below describes the properties of this PIP. A PIP is a short snippet
-of code that can be read by the Projucer and used to generate a JUCE project.
-BEGIN_JUCE_PIP_METADATA
-name:             PluginDemo
-version:          0.0.1
-vendor:           Tracktion
-website:          www.tracktion.com
-description:      This example simply creates a new project and records from all avaliable inputs.
-dependencies:     juce_audio_basics, juce_audio_devices, juce_audio_formats, juce_audio_processors, juce_audio_utils,
-juce_core, juce_data_structures, juce_dsp, juce_events, juce_graphics,
-juce_gui_basics, juce_gui_extra, juce_osc, tracktion_engine
-exporters:        linux_make, vs2017, xcode_iphone, xcode_mac
-moduleFlags:      JUCE_STRICT_REFCOUNTEDPOINTER=1, JUCE_PLUGINHOST_AU=1, JUCE_PLUGINHOST_VST3=1
-type:             Component
-mainClass:        PluginDemo
-END_JUCE_PIP_METADATA
+ The block below describes the properties of this PIP. A PIP is a short snippet
+ of code that can be read by the Projucer and used to generate a JUCE project.
+
+ BEGIN_JUCE_PIP_METADATA
+
+  name:             PluginDemo
+  version:          0.0.1
+  vendor:           Tracktion
+  website:          www.tracktion.com
+  description:      This example simply creates a new project and records from all avaliable inputs.
+
+  dependencies:     juce_audio_basics, juce_audio_devices, juce_audio_formats, juce_audio_processors, juce_audio_utils,
+                    juce_core, juce_data_structures, juce_dsp, juce_events, juce_graphics,
+                    juce_gui_basics, juce_gui_extra, juce_osc, tracktion_engine
+  exporters:        linux_make, vs2017, xcode_iphone, xcode_mac
+
+  moduleFlags:      JUCE_STRICT_REFCOUNTEDPOINTER=1, JUCE_PLUGINHOST_AU=1, JUCE_PLUGINHOST_VST3=1, JUCE_MODAL_LOOPS_PERMITTED=1
+
+  type:             Component
+  mainClass:        PluginDemo
+
+ END_JUCE_PIP_METADATA
+
 *******************************************************************************/
 
 #pragma once
@@ -24,67 +31,67 @@ END_JUCE_PIP_METADATA
 #include "common/PluginWindow.h"
 
 //==============================================================================
-class PluginDemo    : public Component,
-                      private ChangeListener
+class PluginDemo  : public Component,
+                    private ChangeListener
 {
 public:
     //==============================================================================
     PluginDemo()
     {
-        settingsButton.onClick = [this] { EngineHelpers::showAudioDeviceSettings(engine); };
+        settingsButton.onClick  = [this] { EngineHelpers::showAudioDeviceSettings (engine); };
         pluginsButton.onClick = [this]
         {
             DialogWindow::LaunchOptions o;
-            o.dialogTitle = TRANS("Plugins");
-            o.dialogBackgroundColour = Colours::black;
-            o.escapeKeyTriggersCloseButton = true;
-            o.useNativeTitleBar = true;
-            o.resizable = true;
-            o.useBottomRightCornerResizer = true;
-
-            auto v = new PluginListComponent(engine.getPluginManager().pluginFormatManager,
-                engine.getPluginManager().knownPluginList,
-                engine.getTemporaryFileManager().getTempFile("PluginScanDeadMansPedal"),
-                te::getApplicationSettings());
-            v->setSize(800, 600);
-            o.content.setOwned(v);
+            o.dialogTitle                   = TRANS("Plugins");
+            o.dialogBackgroundColour        = Colours::black;
+            o.escapeKeyTriggersCloseButton  = true;
+            o.useNativeTitleBar             = true;
+            o.resizable                     = true;
+            o.useBottomRightCornerResizer   = true;
+            
+            auto v = new PluginListComponent (engine.getPluginManager().pluginFormatManager,
+                                              engine.getPluginManager().knownPluginList,
+                                              engine.getTemporaryFileManager().getTempFile ("PluginScanDeadMansPedal"),
+                                              te::getApplicationSettings());
+            v->setSize (800, 600);
+            o.content.setOwned (v);
             o.launchAsync();
         };
         newEditButton.onClick = [this] { createOrLoadEdit(); };
-
+        
         updatePlayButtonText();
-        editNameLabel.setJustificationType(Justification::centred);
-        Helpers::addAndMakeVisible(*this, { &settingsButton, &pluginsButton, &newEditButton, &playPauseButton, &showEditButton,
-                                    &newTrackButton, &deleteButton, &editNameLabel });
+        editNameLabel.setJustificationType (Justification::centred);
+        Helpers::addAndMakeVisible (*this, { &settingsButton, &pluginsButton, &newEditButton, &playPauseButton, &showEditButton,
+                                             &newTrackButton, &deleteButton, &editNameLabel });
 
-        deleteButton.setEnabled(false);
-
-        auto d = File::getSpecialLocation(File::tempDirectory).getChildFile("PluginDemo");
+        deleteButton.setEnabled (false);
+        
+        auto d = File::getSpecialLocation (File::tempDirectory).getChildFile ("PluginDemo");
         d.createDirectory();
 
-        auto f = Helpers::findRecentEdit(d);
+        auto f = Helpers::findRecentEdit (d);
         if (f.existsAsFile())
-            createOrLoadEdit(f);
+            createOrLoadEdit (f);
         else
-            createOrLoadEdit(d.getNonexistentChildFile("Test", ".tracktionedit", false));
-
-        selectionManager.addChangeListener(this);
-
+            createOrLoadEdit (d.getNonexistentChildFile ("Test", ".tracktionedit", false));
+        
+        selectionManager.addChangeListener (this);
+        
         setupButtons();
-
-        setSize(600, 400);
+        
+        setSize (600, 400);
     }
 
     ~PluginDemo() override
     {
-        te::EditFileOperations(*edit).save(true, true, false);
+        te::EditFileOperations (*edit).save (true, true, false);
         engine.getTemporaryFileManager().getTempDirectory().deleteRecursively();
     }
 
     //==============================================================================
-    void paint(Graphics& g) override
+    void paint (Graphics& g) override
     {
-        g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
+        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
     }
 
     void resized() override
@@ -92,54 +99,54 @@ public:
         auto r = getLocalBounds();
         int w = r.getWidth() / 7;
         auto topR = r.removeFromTop (30);
-        settingsButton.setBounds (topR.removeFromLeft(w).reduced(2));
-        pluginsButton.setBounds (topR.removeFromLeft(w).reduced(2));
-        newEditButton.setBounds (topR.removeFromLeft(w).reduced(2));
-        playPauseButton.setBounds (topR.removeFromLeft(w).reduced(2));
-        showEditButton.setBounds (topR.removeFromLeft(w).reduced(2));
-        newTrackButton.setBounds (topR.removeFromLeft(w).reduced(2));
-        deleteButton.setBounds (topR.removeFromLeft(w).reduced(2));
+        settingsButton.setBounds (topR.removeFromLeft (w).reduced (2));
+        pluginsButton.setBounds (topR.removeFromLeft (w).reduced (2));
+        newEditButton.setBounds (topR.removeFromLeft (w).reduced (2));
+        playPauseButton.setBounds (topR.removeFromLeft (w).reduced (2));
+        showEditButton.setBounds (topR.removeFromLeft (w).reduced (2));
+        newTrackButton.setBounds (topR.removeFromLeft (w).reduced (2));
+        deleteButton.setBounds (topR.removeFromLeft (w).reduced (2));
         topR = r.removeFromTop (30);
         editNameLabel.setBounds (topR);
-
+        
         if (editComponent != nullptr)
-            editComponent->setBounds(r);
+            editComponent->setBounds (r);
     }
 
 private:
     //==============================================================================
-    te::Engine engine{ ProjectInfo::projectName, std::make_unique<ExtendedUIBehaviour>(), nullptr };
-    te::SelectionManager selectionManager{ engine };
+    te::Engine engine { ProjectInfo::projectName, std::make_unique<ExtendedUIBehaviour>(), nullptr };
+    te::SelectionManager selectionManager { engine };
     std::unique_ptr<te::Edit> edit;
     std::unique_ptr<EditComponent> editComponent;
 
-    TextButton settingsButton{ "Settings" }, pluginsButton{ "Plugins" }, newEditButton{ "New" }, playPauseButton{ "Play" },
-        showEditButton{ "Show Edit" }, newTrackButton{ "New Track" }, deleteButton{ "Delete" };
-    Label editNameLabel{ "No Edit Loaded" };
-    ToggleButton showWaveformButton{ "Show Waveforms" };
+    TextButton settingsButton { "Settings" }, pluginsButton { "Plugins" }, newEditButton { "New" }, playPauseButton { "Play" },
+               showEditButton { "Show Edit" }, newTrackButton { "New Track" }, deleteButton { "Delete" };
+    Label editNameLabel { "No Edit Loaded" };
+    ToggleButton showWaveformButton { "Show Waveforms" };
 
     //==============================================================================
     void setupButtons()
     {
         playPauseButton.onClick = [this]
         {
-            EngineHelpers::togglePlay(*edit);
+            EngineHelpers::togglePlay (*edit);
         };
         newTrackButton.onClick = [this]
         {
-            edit->ensureNumberOfAudioTracks(getAudioTracks(*edit).size() + 1);
+            edit->ensureNumberOfAudioTracks (getAudioTracks (*edit).size() + 1);
         };
         deleteButton.onClick = [this]
         {
-            auto sel = selectionManager.getSelectedObject(0);
+            auto sel = selectionManager.getSelectedObject (0);
             if (auto clip = dynamic_cast<te::Clip*> (sel))
             {
                 clip->removeFromParentTrack();
             }
             else if (auto track = dynamic_cast<te::Track*> (sel))
             {
-                if (!(track->isMarkerTrack() || track->isTempoTrack() || track->isChordTrack()))
-                    edit->deleteTrack(track);
+                if (! (track->isMarkerTrack() || track->isTempoTrack() || track->isChordTrack()))
+                    edit->deleteTrack (track);
             }
             else if (auto plugin = dynamic_cast<te::Plugin*> (sel))
             {
@@ -149,90 +156,90 @@ private:
         showWaveformButton.onClick = [this]
         {
             auto& evs = editComponent->getEditViewState();
-            evs.drawWaveforms = !evs.drawWaveforms.get();
-            showWaveformButton.setToggleState(evs.drawWaveforms, dontSendNotification);
+            evs.drawWaveforms = ! evs.drawWaveforms.get();
+            showWaveformButton.setToggleState (evs.drawWaveforms, dontSendNotification);
         };
     }
-
+    
     void updatePlayButtonText()
     {
         if (edit != nullptr)
-            playPauseButton.setButtonText(edit->getTransport().isPlaying() ? "Stop" : "Play");
+            playPauseButton.setButtonText (edit->getTransport().isPlaying() ? "Stop" : "Play");
     }
-
-    void createOrLoadEdit(File editFile = {})
+    
+    void createOrLoadEdit (File editFile = {})
     {
         if (editFile == File())
         {
-            FileChooser fc("New Edit", File::getSpecialLocation(File::userDocumentsDirectory), "*.tracktionedit");
-            if (fc.browseForFileToSave(true))
+            FileChooser fc ("New Edit", File::getSpecialLocation (File::userDocumentsDirectory), "*.tracktionedit");
+            if (fc.browseForFileToSave (true))
                 editFile = fc.getResult();
             else
                 return;
         }
-
+        
         selectionManager.deselectAll();
         editComponent = nullptr;
 
         if (editFile.existsAsFile())
-            edit = te::loadEditFromFile(engine, editFile);
+            edit = te::loadEditFromFile (engine, editFile);
         else
-            edit = te::createEmptyEdit(engine, editFile);
+            edit = te::createEmptyEdit (engine, editFile);
 
         edit->playInStopEnabled = true;
-
+        
         auto& transport = edit->getTransport();
-        transport.addChangeListener(this);
-
-        editNameLabel.setText(editFile.getFileNameWithoutExtension(), dontSendNotification);
+        transport.addChangeListener (this);
+        
+        editNameLabel.setText (editFile.getFileNameWithoutExtension(), dontSendNotification);
         showEditButton.onClick = [this, editFile]
         {
-            te::EditFileOperations(*edit).save(true, true, false);
+            te::EditFileOperations (*edit).save (true, true, false);
             editFile.revealToUser();
         };
-
-        te::EditFileOperations(*edit).save(true, true, false);
-
+        
+        te::EditFileOperations (*edit).save (true, true, false);
+        
         enableAllInputs();
-
-        editComponent = std::make_unique<EditComponent>(*edit, selectionManager);
+        
+        editComponent = std::make_unique<EditComponent> (*edit, selectionManager);
         editComponent->getEditViewState().showFooters = true;
         editComponent->getEditViewState().showMidiDevices = true;
         editComponent->getEditViewState().showWaveDevices = true;
-
-        addAndMakeVisible(*editComponent);
+        
+        addAndMakeVisible (*editComponent);
     }
-
+    
     void enableAllInputs()
     {
         auto& dm = engine.getDeviceManager();
-
+        
         for (int i = 0; i < dm.getNumMidiInDevices(); i++)
         {
-            if (auto mip = dm.getMidiInDevice(i))
+            if (auto mip = dm.getMidiInDevice (i))
             {
-                mip->setEndToEndEnabled(true);
-                mip->setEnabled(true);
+                mip->setEndToEndEnabled (true);
+                mip->setEnabled (true);
             }
         }
-
+        
         for (int i = 0; i < dm.getNumWaveInDevices(); i++)
-            if (auto wip = dm.getWaveInDevice(i))
-                wip->setStereoPair(false);
-
+            if (auto wip = dm.getWaveInDevice (i))
+                wip->setStereoPair (false);
+        
         for (int i = 0; i < dm.getNumWaveInDevices(); i++)
         {
-            if (auto wip = dm.getWaveInDevice(i))
+            if (auto wip = dm.getWaveInDevice (i))
             {
-                wip->setEndToEnd(true);
-                wip->setEnabled(true);
+                wip->setEndToEnd (true);
+                wip->setEnabled (true);
             }
         }
-
+        
         edit->getTransport().ensureContextAllocated();
     }
-
-    void changeListenerCallback(ChangeBroadcaster* source) override
+    
+    void changeListenerCallback (ChangeBroadcaster* source) override
     {
         if (edit != nullptr && source == &edit->getTransport())
         {
@@ -240,12 +247,12 @@ private:
         }
         else if (source == &selectionManager)
         {
-            auto sel = selectionManager.getSelectedObject(0);
-            deleteButton.setEnabled(dynamic_cast<te::Clip*> (sel) != nullptr
-                || dynamic_cast<te::Track*> (sel) != nullptr
-                || dynamic_cast<te::Plugin*> (sel));
+            auto sel = selectionManager.getSelectedObject (0);
+            deleteButton.setEnabled (dynamic_cast<te::Clip*> (sel) != nullptr
+                                     || dynamic_cast<te::Track*> (sel) != nullptr
+                                     || dynamic_cast<te::Plugin*> (sel));
         }
     }
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PluginDemo)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginDemo)
 };
