52ffb424f3311b76740adacd08a761e9323e854f
Jamie Pond <jamiepond259@gmail.com>
2022-10-11 23:23:30 +0100

    add more headless checks

--- modules/tracktion_engine/playback/tracktion_TransportControl.cpp
+++ modules/tracktion_engine/playback/tracktion_TransportControl.cpp
@@ -788,6 +788,10 @@ struct TransportControl::ScreenSaverDefeater
 
     ~ScreenSaverDefeater()
     {
+#if TRACKTION_FORCE_HEADLESS
+        return;
+#endif
+
         if (juce::Desktop::getInstance().isHeadless())
             return;
 
