fc457b01543227c09be44ae629aa2e59c136f591
David Rowland <drowland96@gmail.com>
2023-05-16 11:31:40 +0100

    Transport:   Fixed a problem where the transport position would jump to the loop in when stopped if looping was enabled

--- modules/tracktion_engine/playback/tracktion_TransportControl.cpp
+++ modules/tracktion_engine/playback/tracktion_TransportControl.cpp
@@ -997,9 +997,15 @@ void TransportControl::timerCallback()
          && (! transportState->userDragging)
          && juce::Time::getMillisecondCounter() - transportState->lastUserDragTime > 200)
     {
-        const auto currentTime = playHeadWrapper->getLiveTransportPosition();
-        transportState->setVideoPosition (currentTime, false);
-        transportState->updatePositionFromPlayhead (currentTime);
+        // Only update if we're not looping or we're playing as otherwise the transport
+        // position will jump and be stuck at the loop in position.
+        // The other way to fix this mught be to change the play head to only snap the position on play start..
+        if (! looping || isPlaying())
+        {
+            const auto currentTime = playHeadWrapper->getLiveTransportPosition();
+            transportState->setVideoPosition (currentTime, false);
+            transportState->updatePositionFromPlayhead (currentTime);
+        }
     }
 
     // Periodically update the loop times from the transport state
