65425efe64fa7225106d7c8c638ddd61975fe51b
David Rowland <drowland96@gmail.com>
2023-07-24 17:24:43 +0100

    Launcher:   Added quantised tests

--- modules/tracktion_engine/model/clips/tracktion_LaunchHandle.test.cpp
+++ modules/tracktion_engine/model/clips/tracktion_LaunchHandle.test.cpp
@@ -30,14 +30,13 @@ public:
     {
         auto& engine = *Engine::getEngines()[0];
         runBasicLauchHandleTests (engine);
+        runQuantisedLauchHandleTests (engine);
     }
 
 private:
     void runBasicLauchHandleTests (Engine& engine)
     {
         beginTest ("Non-quantised launching");
-
-        auto sinFile = graph::test_utilities::getSinFile<juce::WavAudioFormat> (44100.0, 5.0, 2, 220.0f);
 
         {
             LaunchHandle h (4_bd);
@@ -55,6 +54,7 @@ private:
                 expect (! h.getQueuedStatus());
                 expectWithinAbsoluteError (*h.getProgress(), 0.125f, 0.001f);
 
+                expect (! s.isSplit);
                 expect (s.playing1);
                 expect (s.range1 == BeatRange (0_bp, 0.5_bp));
                 expect (! s.playing2);
@@ -67,6 +67,7 @@ private:
                 expect (! h.getQueuedStatus());
                 expectWithinAbsoluteError (*h.getProgress(), 0.25f, 0.001f);
 
+                expect (! s.isSplit);
                 expect (s.playing1);
                 expect (s.range1 == BeatRange (0.5_bp, 1.0_bp));
                 expect (! s.playing2);
@@ -83,10 +84,70 @@ private:
                 expect (! h.getQueuedStatus());
                 expect (! h.getProgress());
 
+                expect (! s.isSplit);
                 expect (! s.playing1);
                 expect (s.range1 == BeatRange (1.0_bp, 1.5_bp));
                 expect (! s.playing2);
                 expect (s.range2.isEmpty());
+            }
+        }
+    }
+
+    void runQuantisedLauchHandleTests (Engine& engine)
+    {
+        beginTest ("Quantised launching");
+
+        {
+            LaunchHandle h (4_bd);
+            expect (h.getPlayingStatus() == LaunchHandle::PlayState::stopped);
+            expect (! h.getQueuedStatus());
+            expect (! h.getProgress());
+
+            h.play (0.25_bp);
+            expect (h.getPlayingStatus() == LaunchHandle::PlayState::stopped);
+            expect (h.getQueuedStatus() == LaunchHandle::QueueState::playQueued);
+
+            {
+                auto s = h.update ({ 0_bp, 0.5_bp });
+                expect (h.getPlayingStatus() == LaunchHandle::PlayState::playing);
+                expect (! h.getQueuedStatus());
+                expectWithinAbsoluteError (*h.getProgress(), 0.0625f, 0.001f);
+
+                expect (s.isSplit);
+                expect (! s.playing1);
+                expect (s.range1 == BeatRange (0_bp, 0.25_bp));
+                expect (s.playing2);
+                expect (s.range2 == BeatRange (0.25_bp, 0.5_bp));
+            }
+
+            {
+                auto s = h.update ({ 0.5_bp, 1.0_bp });
+                expect (h.getPlayingStatus() == LaunchHandle::PlayState::playing);
+                expect (! h.getQueuedStatus());
+                expectWithinAbsoluteError (*h.getProgress(), 0.1875f, 0.001f);
+
+                expect (! s.isSplit);
+                expect (s.playing1);
+                expect (s.range1 == BeatRange (0.5_bp, 1.0_bp));
+                expect (! s.playing2);
+                expect (s.range2.isEmpty());
+            }
+
+            h.stop (1.25_bp);
+            expect (h.getQueuedStatus() == LaunchHandle::QueueState::stopQueued);
+            expect (h.getPlayingStatus() == LaunchHandle::PlayState::playing);
+
+            {
+                auto s = h.update ({ 1.0_bp, 1.5_bp });
+                expect (h.getPlayingStatus() == LaunchHandle::PlayState::stopped);
+                expect (! h.getQueuedStatus());
+                expect (! h.getProgress());
+
+                expect (s.isSplit);
+                expect (s.playing1);
+                expect (s.range1 == BeatRange (1.0_bp, 1.25_bp));
+                expect (! s.playing2);
+                expect (s.range2 == BeatRange (1.25_bp, 1.5_bp));
             }
         }
     }
