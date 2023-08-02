b0647978a0dd962e9568bf96f06f6b54f07827c8
Liam Gallagher <liam.gallagher@beatconnect.com>
2023-08-02 15:39:07 -0400

    better results for ranged items in getMidiNote

--- modules/tracktion_engine/midi/bc_Midi.h
+++ modules/tracktion_engine/midi/bc_Midi.h
@@ -217,13 +217,24 @@ namespace tracktion { inline namespace engine { namespace BeatConnect
 
 				static juce::String getNoteName(double p_hz)
 				{
-					for (auto element : midiNoteValues)
+					for (auto it = midiNoteValues.begin(); it != midiNoteValues.end(); it++)
 					{
-						if (element.freqHz == p_hz)
-							return element.noteName;
-						if ((&element - 1) != nullptr && (&element + 1) != nullptr)
-							if (p_hz > (&element - 1)->freqHz && p_hz < (&element + 1)->freqHz)
-								return element.noteName;
+						if (it->freqHz == p_hz)
+							return it->noteName;
+						if (it > midiNoteValues.begin() && it < midiNoteValues.end()) {
+							if (p_hz > (it - 1)->freqHz && p_hz < (it + 1)->freqHz)
+							{
+								return it->noteName;
+							}
+							else if (p_hz < midiNoteValues.begin()->freqHz)
+							{
+								return midiNoteValues.begin()->noteName;
+							}
+							else if (p_hz > midiNoteValues.end()->freqHz)
+							{
+								midiNoteValues.end()->noteName;
+							}
+						}
 					}
 					return "";
 				}
