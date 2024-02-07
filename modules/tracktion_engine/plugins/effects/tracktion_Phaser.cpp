06fc2c277187d09edb61524784637279986054ad
Liam Gallagher <133661475+LiamGallagherBC@users.noreply.github.com>
2024-02-07 14:58:56 -0500

    1681 - Phaser Upgrades and Restoration of te::PhaserPlugin (#43)
    * add bc_PhaserPlugin
    
    * Restore te::PhaserPlugin to stock

--- modules/tracktion_engine/plugins/effects/tracktion_Phaser.cpp
+++ modules/tracktion_engine/plugins/effects/tracktion_Phaser.cpp
@@ -8,142 +8,125 @@
     Tracktion Engine uses a GPL/commercial licence - see LICENCE.md for details.
 */
 
-namespace tracktion { inline namespace engine
-{
-
-PhaserPlugin::PhaserPlugin (PluginCreationInfo info)  : Plugin (info)
-{
-    auto um = getUndoManager();
-
-    // BEATCONNECT MODIFICATION START
-    // This appears missing for some reason?
-    depthParam = addParam("depth", TRANS("Depth"), { 0, 20.0f });
-    rateParam = addParam("rate", TRANS("Rate"), { 0.1f, 20.0f });
-    feedbackGainParam = addParam("feedback", TRANS("Feedback"), { 0.0f, 0.7f });
-    // BEATCONNECT MODIFICATION END
-
-    depth.referTo (state, IDs::depth, um, 5.0f);
-    rate.referTo (state, IDs::rate, um, 0.4f);
-    feedbackGain.referTo (state, IDs::feedback, um, 0.7f);
-
-    // BEATCONNECT MODIFICATION START
-    depthParam->attachToCurrentValue(depth);
-    rateParam->attachToCurrentValue(rate);
-    feedbackGainParam->attachToCurrentValue(feedbackGain);
-    // BEATCONNECT MODIFICATION END
-}
-
-PhaserPlugin::~PhaserPlugin()
-{
-    notifyListenersOfDeletion();
-
-    // BEATCONNECT MODIFICATION START
-    depthParam->detachFromCurrentValue();
-    rateParam->detachFromCurrentValue();
-    feedbackGainParam->detachFromCurrentValue();
-    // BEATCONNECT MODIFICATION END
-}
-
-const char* PhaserPlugin::xmlTypeName = "phaser";
-// BEATCONNECT MODIFICATION START
-const char* PhaserPlugin::uniqueId = "bb7d2f63-861a-433a-9559-48696bd38851";
-// BEATCONNECT MODIFICATION END
-
-void PhaserPlugin::initialise (const PluginInitialisationInfo& info)
-{
-    sampleRate = info.sampleRate;
-    std::memset (filterVals, 0, sizeof (filterVals));
-
-    const float delayMs = 100.0f;
-    sweep = minSweep = (juce::MathConstants<double>::pi * delayMs) / sampleRate;
-    sweepFactor = 1.001f;
-}
-
-void PhaserPlugin::deinitialise()
-{
-}
-
-void PhaserPlugin::applyToBuffer (const PluginRenderContext& fc)
-{
-    if (fc.destBuffer == nullptr)
-        return;
-
-    SCOPED_REALTIME_CHECK
-
-    const double range = pow (2.0, (double) depth);
-    const double sweepUp = pow (range, rate / (sampleRate / 2));
-    const double sweepDown = 1.0 / sweepUp;
-
-    const float delayMs = 100.0f;
-    const float maxSweep = (float) ((juce::MathConstants<double>::pi * delayMs * range) / sampleRate);
-
-    double swpFactor = sweepFactor > 1.0 ? sweepUp : sweepDown;
-    double swp = sweep;
-
-    clearChannels (*fc.destBuffer, 2, -1, fc.bufferStartSample, fc.bufferNumSamples);
-
-    for (int chan = std::min (2, fc.destBuffer->getNumChannels()); --chan >= 0;)
+namespace tracktion {
+    inline namespace engine
     {
-        float* b = fc.destBuffer->getWritePointer (chan, fc.bufferStartSample);
-        swp = sweep;
-        swpFactor = sweepFactor;
 
-        for (int i = fc.bufferNumSamples; --i >= 0;)
+        PhaserPlugin::PhaserPlugin(PluginCreationInfo info) : Plugin(info)
         {
-            float inval = *b;
-            const double coef = (1.0 - swp) / (1.0 + swp);
-            double* const fv = filterVals[chan];
+            auto um = getUndoManager();
 
-            double t = inval + feedbackGain * fv[7];
-
-            JUCE_UNDENORMALISE (t);
-
-            fv[1] = coef * (fv[1] + t) - fv[0];
-            JUCE_UNDENORMALISE (fv[1]);
-            fv[0] = t;
-
-            fv[3] = coef * (fv[3] + fv[1]) - fv[2];
-            JUCE_UNDENORMALISE (fv[3]);
-            fv[2] = fv[1];
-
-            fv[5] = coef * (fv[5] + fv[3]) - fv[4];
-            JUCE_UNDENORMALISE (fv[5]);
-            fv[4] = fv[3];
-
-            fv[7] = coef * (fv[7] + fv[5]) - fv[6];
-            JUCE_UNDENORMALISE (fv[7]);
-            fv[6] = fv[5];
-
-            inval += (float)fv[7];
-            JUCE_UNDENORMALISE (inval);
-
-            *b++ = inval;
-
-            swp *= swpFactor;
-
-            if (swp > maxSweep)       swpFactor = sweepDown;
-            else if (swp < minSweep)  swpFactor = sweepUp;
+            depth.referTo(state, IDs::depth, um, 5.0f);
+            rate.referTo(state, IDs::rate, um, 0.4f);
+            feedbackGain.referTo(state, IDs::feedback, um, 0.7f);
         }
+
+        PhaserPlugin::~PhaserPlugin()
+        {
+            notifyListenersOfDeletion();
+        }
+
+        const char* PhaserPlugin::xmlTypeName = "phaser";
+        // BEATCONNECT MODIFICATION START
+        const char* PhaserPlugin::uniqueId = "5b9ba56c-e53a-48b3-b70b-fb930fd9c00d";
+        // BEATCONNECT MODIFICATION END
+
+        void PhaserPlugin::initialise(const PluginInitialisationInfo& info)
+        {
+            sampleRate = info.sampleRate;
+            std::memset(filterVals, 0, sizeof(filterVals));
+
+            const float delayMs = 100.0f;
+            sweep = minSweep = (juce::MathConstants<double>::pi * delayMs) / sampleRate;
+            sweepFactor = 1.001f;
+        }
+
+        void PhaserPlugin::deinitialise()
+        {
+        }
+
+        void PhaserPlugin::applyToBuffer(const PluginRenderContext& fc)
+        {
+            if (fc.destBuffer == nullptr)
+                return;
+
+            SCOPED_REALTIME_CHECK
+
+                const double range = pow(2.0, (double)depth);
+            const double sweepUp = pow(range, rate / (sampleRate / 2));
+            const double sweepDown = 1.0 / sweepUp;
+
+            const float delayMs = 100.0f;
+            const float maxSweep = (float)((juce::MathConstants<double>::pi * delayMs * range) / sampleRate);
+
+            double swpFactor = sweepFactor > 1.0 ? sweepUp : sweepDown;
+            double swp = sweep;
+
+            clearChannels(*fc.destBuffer, 2, -1, fc.bufferStartSample, fc.bufferNumSamples);
+
+            for (int chan = std::min(2, fc.destBuffer->getNumChannels()); --chan >= 0;)
+            {
+                float* b = fc.destBuffer->getWritePointer(chan, fc.bufferStartSample);
+                swp = sweep;
+                swpFactor = sweepFactor;
+
+                for (int i = fc.bufferNumSamples; --i >= 0;)
+                {
+                    float inval = *b;
+                    const double coef = (1.0 - swp) / (1.0 + swp);
+                    double* const fv = filterVals[chan];
+
+                    double t = inval + feedbackGain * fv[7];
+
+                    JUCE_UNDENORMALISE(t);
+
+                    fv[1] = coef * (fv[1] + t) - fv[0];
+                    JUCE_UNDENORMALISE(fv[1]);
+                    fv[0] = t;
+
+                    fv[3] = coef * (fv[3] + fv[1]) - fv[2];
+                    JUCE_UNDENORMALISE(fv[3]);
+                    fv[2] = fv[1];
+
+                    fv[5] = coef * (fv[5] + fv[3]) - fv[4];
+                    JUCE_UNDENORMALISE(fv[5]);
+                    fv[4] = fv[3];
+
+                    fv[7] = coef * (fv[7] + fv[5]) - fv[6];
+                    JUCE_UNDENORMALISE(fv[7]);
+                    fv[6] = fv[5];
+
+                    inval += (float)fv[7];
+                    JUCE_UNDENORMALISE(inval);
+
+                    *b++ = inval;
+
+                    swp *= swpFactor;
+
+                    if (swp > maxSweep)       swpFactor = sweepDown;
+                    else if (swp < minSweep)  swpFactor = sweepUp;
+                }
+            }
+
+            zeroDenormalisedValuesIfNeeded(*fc.destBuffer);
+
+            sweep = swp;
+            sweepFactor = swpFactor;
+        }
+
+        juce::String PhaserPlugin::getSelectableDescription()
+        {
+            return TRANS("Phaser Plugin");
+        }
+
+        void PhaserPlugin::restorePluginStateFromValueTree(const juce::ValueTree& v)
+        {
+            juce::CachedValue<float>* cvsFloat[] = { &depth, &rate, &feedbackGain, nullptr };
+            copyPropertiesToNullTerminatedCachedValues(v, cvsFloat);
+
+            for (auto p : getAutomatableParameters())
+                p->updateFromAttachedValue();
+        }
+
     }
-
-    zeroDenormalisedValuesIfNeeded (*fc.destBuffer);
-
-    sweep = swp;
-    sweepFactor = swpFactor;
-}
-
-juce::String PhaserPlugin::getSelectableDescription()
-{
-    return TRANS("Phaser Plugin");
-}
-
-void PhaserPlugin::restorePluginStateFromValueTree (const juce::ValueTree& v)
-{
-    juce::CachedValue<float>* cvsFloat[]  = { &depth, &rate, &feedbackGain, nullptr };
-    copyPropertiesToNullTerminatedCachedValues (v, cvsFloat);
-
-    for (auto p : getAutomatableParameters())
-        p->updateFromAttachedValue();
-}
-
-}} // namespace tracktion { inline namespace engine
+} // namespace tracktion { inline namespace engine
--- modules/tracktion_engine/plugins/effects/tracktion_Phaser.h
+++ modules/tracktion_engine/plugins/effects/tracktion_Phaser.h
@@ -8,46 +8,46 @@
     Tracktion Engine uses a GPL/commercial licence - see LICENCE.md for details.
 */
 
-namespace tracktion { inline namespace engine
-{
+namespace tracktion {
+    inline namespace engine
+    {
 
-class PhaserPlugin  : public Plugin
-{
-public:
-    PhaserPlugin (PluginCreationInfo);
-    ~PhaserPlugin() override;
+        class PhaserPlugin : public Plugin
+        {
+        public:
+            PhaserPlugin(PluginCreationInfo);
+            ~PhaserPlugin() override;
 
-    //==============================================================================
-    static const char* getPluginName()                      { return NEEDS_TRANS("Phaser"); }
-    static const char* xmlTypeName;
-    // BEATCONNECT MODIFICATION START
-    static const char* uniqueId;
-    // BEATCONNECT MODIFICATION END
+            //==============================================================================
+            static const char* getPluginName() { return NEEDS_TRANS("Phaser"); }
+            static const char* xmlTypeName;
+            // BEATCONNECT MODIFICATION START
+            static const char* uniqueId;
+            // BEATCONNECT MODIFICATION START
 
-    juce::String getName() override                         { return TRANS("Phaser"); }
-    juce::String getPluginType() override                   { return xmlTypeName; }
-    bool needsConstantBufferSize() override                 { return false; }
-    void initialise (const PluginInitialisationInfo&) override;
-    void deinitialise() override;
-    int getNumOutputChannelsGivenInputs (int numInputChannels) override  { return juce::jmin (numInputChannels, 2); }
-    void applyToBuffer (const PluginRenderContext&) override;
-    juce::String getSelectableDescription() override;
-    // BEATCONNECT MODIFICATION START
-    juce::String getUniqueId() override                     { return uniqueId; }
-    // BEATCONNECT MODIFICATION END
-    void restorePluginStateFromValueTree (const juce::ValueTree&) override;
+            juce::String getName() override { return TRANS("Phaser"); }
+            // BEATCONNECT MODIFICATION START
+            juce::String getUniqueId() override                     { return uniqueId; }
+            // BEATCONNECT MODIFICATION END
+            juce::String getPluginType() override { return xmlTypeName; }
+            bool needsConstantBufferSize() override { return false; }
+            void initialise(const PluginInitialisationInfo&) override;
+            void deinitialise() override;
+            int getNumOutputChannelsGivenInputs(int numInputChannels) override { return juce::jmin(numInputChannels, 2); }
+            void applyToBuffer(const PluginRenderContext&) override;
+            juce::String getSelectableDescription() override;
+            void restorePluginStateFromValueTree(const juce::ValueTree&) override;
 
-    juce::CachedValue<float> depth, rate, feedbackGain;
-    // BEATCONNECT MODIFICATION START
-    AutomatableParameter::Ptr depthParam, rateParam, feedbackGainParam;
-    // BEATCONNECT MODIFICATION END
 
-private:
-    //==============================================================================
-    double filterVals[2][8];
-    double sweep = 0.0, sweepFactor = 0.0, minSweep = 0.0;
+            juce::CachedValue<float> depth, rate, feedbackGain;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PhaserPlugin)
-};
+        private:
+            //==============================================================================
+            double filterVals[2][8];
+            double sweep = 0.0, sweepFactor = 0.0, minSweep = 0.0;
 
-}} // namespace tracktion { inline namespace engine
+            JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PhaserPlugin)
+        };
+
+    }
+} // namespace tracktion { inline namespace engine
