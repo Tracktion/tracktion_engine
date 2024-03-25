09075fffe58ef50eb826d33bd993900b4237bb37
nlaroche <larochemusic@gmail.com>
2024-03-25 09:42:24 -0400

    update default pattern values

--- modules/tracktion_engine/model/clips/tracktion_StepClip.cpp
+++ modules/tracktion_engine/model/clips/tracktion_StepClip.cpp
@@ -657,8 +657,8 @@ int StepClip::insertNewPattern (int index)
 {
     // BEATCONNECT MODIFICATION START
     // Add a little more resolution
-    auto numNotes = getBeatsPerBar() * 16;
-    auto noteLength = 0.25f / 4;
+    auto numNotes = getBeatsPerBar() * 4;
+    auto noteLength = 0.25f;
     auto v = createValueTree (IDs::PATTERN,
                               IDs::numNotes, numNotes,
                               IDs::noteLength, noteLength);
