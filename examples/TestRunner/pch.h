7aecf963942b82fea3d02760824d1e4cc81d06e1
David Rowland <drowland96@gmail.com>
2024-07-18 12:08:34 +0100

    PCH:   Added pch to TestRunner

---
+++ examples/TestRunner/pch.h
@@ -0,0 +1,26 @@
+/*==============================================================================
+
+  Copyright 2016 by Tracktion Corporation.
+  For more information visit www.tracktion.com
+
+ ==============================================================================*/
+
+#pragma once
+
+#include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_core/juce_core.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_events/juce_events.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_gui_extra/juce_gui_extra.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_graphics/juce_graphics.h>
+#include <juce_data_structures/juce_data_structures.h>
+#include <juce_dsp/juce_dsp.h>
+#include <juce_osc/juce_osc.h>
+
+#include <tracktion_core/tracktion_core.h>
+#include <tracktion_graph/tracktion_graph.h>
+#include <tracktion_engine/tracktion_engine.h>
