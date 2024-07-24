7a68773f283c62f295fddf573984d50f37d33596
David Rowland <drowland96@gmail.com>
2024-07-24 16:56:22 +0100

    Hosted audio device:   Added the ability to apply input and output latency for testing purposes

--- modules/tracktion_engine/playback/devices/tracktion_WaveInputDevice.test.cpp
+++ modules/tracktion_engine/playback/devices/tracktion_WaveInputDevice.test.cpp
@@ -18,7 +18,7 @@ namespace tracktion::inline engine
 #if ENGINE_UNIT_TESTS_WAVE_INPUT_DEVICE
     TEST_SUITE ("tracktion_engine")
     {
-        TEST_CASE ("WaveInputDevice")
+        TEST_CASE ("WaveInputDevice: No device latency")
         {
             auto& engine = *Engine::getEngines()[0];
             test_utilities::EnginePlayer player (engine, { .sampleRate = 44100.0, .blockSize = 512, .inputChannels = 1, .outputChannels = 1,
@@ -52,6 +52,122 @@ namespace tracktion::inline engine
             CHECK (graph::test_utilities::buffersAreEqual (recordedFileBuffer, squareBuffer,
                                                            juce::Decibels::decibelsToGain (-99.0f)));
         }
+
+        TEST_CASE ("WaveInputDevice: Device input latency")
+        {
+            constexpr int inputLatencyNumSamples = 1000;
+            auto& engine = *Engine::getEngines()[0];
+            test_utilities::EnginePlayer player (engine, { .sampleRate = 44100.0, .blockSize = 512,
+                                                           .inputChannels = 1, .outputChannels = 1,
+                                                           .inputNames = {}, .outputNames = {},
+                                                           .inputLatencyNumSamples = inputLatencyNumSamples });
+
+            auto edit = engine::test_utilities::createTestEdit (engine, 1, Edit::EditRole::forEditing);
+            auto& tc = edit->getTransport();
+            tc.ensureContextAllocated();
+
+            auto squareFile = graph::test_utilities::getSquareFile<juce::WavAudioFormat> (44100.0, 5.0, 1);
+            auto squareBuffer = *engine::test_utilities::loadFileInToBuffer (engine, squareFile->getFile());
+            AudioFile af (engine, squareFile->getFile());
+
+            auto& destTrack = *getAudioTracks (*edit)[0];
+            auto destAssignment = edit->getCurrentPlaybackContext()->getAllInputs()[0]->setTarget (destTrack.itemID, true, nullptr);
+            (*destAssignment)->recordEnabled = true;
+            edit->dispatchPendingUpdatesSynchronously();
+
+            test_utilities::TempCurrentWorkingDirectory tempDir;
+            tc.record (false);
+
+            player.process (squareBuffer);
+
+            // Flush the output as the first 1000 samples will be discarded by the recording
+            // This wouldn't normally happen, you'd just end up with the last 1000 samples missing from the recording
+            player.process (inputLatencyNumSamples);
+
+            tc.stop (false, true);
+
+            auto recordedClip = dynamic_cast<WaveAudioClip*> (destTrack.getClips()[0]);
+            CHECK(recordedClip);
+            auto recordedFile = recordedClip->getSourceFileReference().getFile();
+            auto recordedFileBuffer = *engine::test_utilities::loadFileInToBuffer (engine, recordedFile);
+
+            CHECK_EQ (squareBuffer.getNumSamples(), recordedFileBuffer.getNumSamples());
+            CHECK (graph::test_utilities::buffersAreEqual (recordedFileBuffer, squareBuffer,
+                                                           juce::Decibels::decibelsToGain (-99.0f)));
+        }
+
+        // TEST_CASE ("WaveInputDevice: Device round-trip latency")
+        // {
+        //     using namespace choc::buffer;
+        //     constexpr int inputLatencyNumSamples = 1000;
+        //     constexpr int outputLatencyNumSamples = 250;
+        //     constexpr int blockSize = 512;
+        //     constexpr auto blockNumFrames = static_cast<FrameCount> (blockSize);
+        //     auto& engine = *Engine::getEngines()[0];
+        //     test_utilities::EnginePlayer player (engine, { .sampleRate = 44100.0, .blockSize = blockSize,
+        //                                                    .inputChannels = 1, .outputChannels = 1,
+        //                                                    .inputNames = {}, .outputNames = {},
+        //                                                    .inputLatencyNumSamples = inputLatencyNumSamples,
+        //                                                    .outputLatencyNumSamples = outputLatencyNumSamples });
+        //
+        //     auto edit = engine::test_utilities::createTestEdit (engine, 2, Edit::EditRole::forEditing);
+        //     auto& tc = edit->getTransport();
+        //     tc.ensureContextAllocated();
+        //
+        //     auto squareFile = graph::test_utilities::getSquareFile<juce::WavAudioFormat> (44100.0, 5.0, 1);
+        //     auto squareBuffer = *engine::test_utilities::loadFileInToBuffer (engine, squareFile->getFile());
+        //     AudioFile af (engine, squareFile->getFile());
+        //     auto& sourceTrack = *getAudioTracks (*edit)[0];
+        //
+        //     auto clip = insertWaveClip (sourceTrack, {}, af.getFile(), { { 0_tp, 5_tp } }, DeleteExistingClips::no);
+        //     clip->setUsesProxy (false);
+        //
+        //     auto& destTrack = *getAudioTracks (*edit)[1];
+        //     auto destAssignment = edit->getCurrentPlaybackContext()->getAllInputs()[0]->setTarget (destTrack.itemID, true, nullptr);
+        //     (*destAssignment)->recordEnabled = true;
+        //     (*destAssignment)->input.owner.setMonitorMode (InputDevice::MonitorMode::off);
+        //     edit->dispatchPendingUpdatesSynchronously();
+        //
+        //     test_utilities::TempCurrentWorkingDirectory tempDir;
+        //     tc.record (false);
+        //
+        //     {
+        //         const auto numFramesToProcess = toBufferView (squareBuffer).getNumFrames()
+        //                                           + static_cast<FrameCount> (inputLatencyNumSamples + outputLatencyNumSamples + blockNumFrames);
+        //         FrameCount startFrame = 0;
+        //         ChannelArrayBuffer<float> scratchBuffer (Size::create (1, blockSize));
+        //         scratchBuffer.clear();
+        //
+        //         for (;;)
+        //         {
+        //             const auto numFramesLeft = numFramesToProcess - startFrame;
+        //
+        //             if (numFramesLeft == 0)
+        //                 break;
+        //
+        //             const auto numFramesThisTime = std::min (scratchBuffer.getNumFrames(), numFramesLeft);
+        //
+        //             auto blockInput = scratchBuffer.getStart (numFramesThisTime);
+        //             auto bufferInput = toAudioBuffer (blockInput);
+        //             auto bufferOutput = player.process (bufferInput);
+        //             copyIntersectionAndClearOutside (scratchBuffer, toBufferView (bufferOutput));
+        //
+        //             startFrame += numFramesThisTime;
+        //         }
+        //     }
+        //
+        //     tc.stop (false, true);
+        //
+        //     auto recordedClip = dynamic_cast<WaveAudioClip*> (destTrack.getClips()[0]);
+        //     CHECK(recordedClip);
+        //     auto recordedFile = recordedClip->getSourceFileReference().getFile();
+        //     auto recordedFileBuffer = *engine::test_utilities::loadFileInToBuffer (engine, recordedFile);
+        //
+        //     CHECK_EQ (squareBuffer.getNumSamples(), recordedFileBuffer.getNumSamples());
+        //     CHECK (graph::test_utilities::buffersAreEqual (toBufferView (recordedFileBuffer).fromFrame (blockNumFrames),
+        //                                                    toBufferView (squareBuffer).fromFrame (blockNumFrames),
+        //                                                    juce::Decibels::decibelsToGain (-99.0f)));
+        // }
 
         TEST_CASE ("WaveInputDevice: Track Device")
         {
