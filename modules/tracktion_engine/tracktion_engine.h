c20f456cd3f6112d352b7c2052a0a5d42335bcf9
David Rowland <drowland96@gmail.com>
2025-02-10 15:34:38 +0000

    Removed an unused header

--- modules/tracktion_engine/tracktion_engine.h
+++ modules/tracktion_engine/tracktion_engine.h
@@ -53,7 +53,6 @@
 #include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_dsp/juce_dsp.h>
 #include <juce_osc/juce_osc.h>
-#include <juce_gui_extra/juce_gui_extra.h>
 
 #if __has_include(<choc/audio/choc_SampleBuffers.h>)
  #include <choc/audio/choc_SampleBuffers.h>
