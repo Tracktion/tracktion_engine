b050ebcc002268a5a66d48575bab647a213a4b07
David Rowland <drowland96@gmail.com>
2025-03-11 12:27:54 +0000

    Auto curves:   Added tests for relative and scale curves

--- modules/tracktion_engine/model/automation/tracktion_AutomationCurveList.test.cpp
+++ modules/tracktion_engine/model/automation/tracktion_AutomationCurveList.test.cpp
@@ -17,56 +17,72 @@
 namespace tracktion::inline engine
 {
 
-TEST_SUITE("tracktion_engine")
-{
-    TEST_CASE("AutomationCurveList")
+    struct AutomationCurveListTestContext
     {
-        auto& engine = *tracktion::engine::Engine::getEngines()[0];
+        static std::unique_ptr<AutomationCurveListTestContext> create (Engine& engine)
+        {
+            auto context = std::make_unique<AutomationCurveListTestContext>();
 
-        auto edit = test_utilities::createTestEdit (engine);
+            context->edit = test_utilities::createTestEdit (engine);
 
-        // Sin file must outlive everything!
-        auto fileLength = 5_td;
-        auto squareFile = graph::test_utilities::getSquareFile<juce::WavAudioFormat> (44100.0, fileLength.inSeconds());
+            // Sin file must outlive everything!
+            auto fileLength = 5_td;
+            context->squareFile = graph::test_utilities::getSquareFile<juce::WavAudioFormat> (44100.0, fileLength.inSeconds());
+            context->track = getAudioTracks (*context->edit)[0];
+            context->clip = insertWaveClip (*context->track, {}, context->squareFile->getFile(), { .time = { 0_tp, fileLength } },
+                                           DeleteExistingClips::no);
 
+            context->volPlugin = context->track->getVolumePlugin();
+            context->volParam = context->volPlugin->volParam;
+            context->volPlugin->setSliderPos (0.0f);
+
+            return context;
+        }
+
+        std::unique_ptr<Edit> edit;
+        std::unique_ptr<juce::TemporaryFile> squareFile;
         AudioTrack::Ptr track;
         WaveAudioClip::Ptr clip;
 
-        {
-            track = getAudioTracks (*edit)[0];
-            clip = insertWaveClip (*track, {}, squareFile->getFile(), { .time = { 0_tp, fileLength } },
-                                   DeleteExistingClips::no);
-            CHECK(clip);
+        VolumeAndPanPlugin::Ptr volPlugin;
+        AutomatableParameter::Ptr volParam;
+    };
 
-            auto tempSourceRender = test_utilities::renderToAudioBuffer (*edit);
+TEST_SUITE("tracktion_engine")
+{
+    TEST_CASE("AutomationCurveList: absolute")
+    {
+        auto& engine = *tracktion::engine::Engine::getEngines()[0];
+        auto context = AutomationCurveListTestContext::create (engine);
+
+        {
+            CHECK(context->clip);
+
+            auto tempSourceRender = test_utilities::renderToAudioBuffer (*context->edit);
             CHECK (test_utilities::getRMSLevel (tempSourceRender, { 0_tp, 5_tp }, 0)
-                    == doctest::Approx (1.0f));
+                    == doctest::Approx (0.0f));
         }
 
         // Construct curve
         {
-            CHECK(! clip->getAutomationCurveList (false));
+            CHECK(! context->clip->getAutomationCurveList (false));
 
-            auto curveList = clip->getAutomationCurveList (true);
+            auto curveList = context->clip->getAutomationCurveList (true);
             CHECK(curveList);
 
-            auto volPlugin = track->getVolumePlugin();
-            auto volParam = volPlugin->volParam;
-            volPlugin->setSliderPos (0.0f);
-
-            CHECK(! volParam->isAutomationActive());
-            auto curveMod = curveList->addCurve (*volParam);
+            CHECK(! context->volParam->isAutomationActive());
+            auto curveMod = curveList->addCurve (*context->volParam);
             CHECK(curveMod);
             CHECK_EQ(curveList->getItems().size(), 1);
 
             auto& curve = curveMod->getCurve (CurveModifierType::absolute).curve;
             curve.addPoint (2.5_bp, 1.0f, 0.0, nullptr);
             curve.addPoint (2.5_bp, 0.0f, 0.0, nullptr); // Sharp square from 1-0 at 2.5b
-            CHECK(volParam->isAutomationActive());
+            CHECK(context->volParam->isAutomationActive());
 
-            volParam->updateStream(); //ddd shouldn't need this!
+            context->volParam->updateStream(); //ddd shouldn't need this!
 
-            auto tempSourceRender = test_utilities::renderToAudioBuffer (*edit);
+            auto tempSourceRender = test_utilities::renderToAudioBuffer (*context->edit);
             CHECK (test_utilities::getRMSLevel (tempSourceRender, { 0_tp, 2_tp }, 0)
                     == doctest::Approx (1.0f).epsilon (0.01f)); // First 2s full vol
             CHECK (test_utilities::getRMSLevel (tempSourceRender, { 3_tp, 5_tp }, 0)
@@ -75,13 +91,13 @@ TEST_SUITE("tracktion_engine")
 
         // Move clip to 1s
         {
-            clip->setStart (1s, false, true);
+            context->clip->setStart (1s, false, true);
 
-            auto volParam = track->getVolumePlugin()->volParam;
+            auto volParam = context->track->getVolumePlugin()->volParam;
             volParam->updateStream(); //ddd shouldn't need this!
 
             // Curve should move with clip
-            auto tempSourceRender = test_utilities::renderToAudioBuffer (*edit);
+            auto tempSourceRender = test_utilities::renderToAudioBuffer (*context->edit);
             CHECK (test_utilities::getRMSLevel (tempSourceRender, { 0_tp, 1_tp }, 0)
                     == doctest::Approx (0.0f)); // No clip, should use default automation value
             CHECK (test_utilities::getRMSLevel (tempSourceRender, { 1_tp, 3_tp }, 0)
@@ -92,17 +108,17 @@ TEST_SUITE("tracktion_engine")
 
         // Delete and reload the Edit and check final state again
         {
-            auto pid = edit->getProjectItemID();
-            auto editState = edit->state;
-            track.reset();
-            clip.reset();
-            edit.reset();
+            auto pid = context->edit->getProjectItemID();
+            auto editState = context->edit->state;
+            auto squareFile = std::move (context->squareFile);
+            context = std::make_unique<AutomationCurveListTestContext>();
 
-            edit = Edit::createEdit ({ engine, editState, pid });
-            track = getAudioTracks (*edit)[0];
-            clip = dynamic_cast<WaveAudioClip*> (track->getClips()[0]);
+            context->squareFile = std::move (squareFile);
+            context->edit = Edit::createEdit ({ engine, editState, pid });
+            context->track = getAudioTracks (*context->edit)[0];
+            context->clip = dynamic_cast<WaveAudioClip*> (context->track->getClips()[0]);
 
-            auto tempSourceRender = test_utilities::renderToAudioBuffer (*edit);
+            auto tempSourceRender = test_utilities::renderToAudioBuffer (*context->edit);
             CHECK (test_utilities::getRMSLevel (tempSourceRender, { 0_tp, 1_tp }, 0)
                    == doctest::Approx (0.0f)); // No clip, should use default automation value
             CHECK (test_utilities::getRMSLevel (tempSourceRender, { 1_tp, 3_tp }, 0)
@@ -113,19 +129,79 @@ TEST_SUITE("tracktion_engine")
 
         // Remove the curve
         {
-            auto volParam = track->getVolumePlugin()->volParam;
+            auto volParam = context->track->getVolumePlugin()->volParam;
 
-            auto curveList = clip->getAutomationCurveList (false);
+            auto curveList = context->clip->getAutomationCurveList (false);
             CHECK(curveList);
             CHECK_EQ(curveList->getItems().size(), 1);
 
             curveList->getItems()[0]->remove();
             CHECK_EQ(curveList->getItems().size(), 0);
 
-            auto tempSourceRender = test_utilities::renderToAudioBuffer (*edit);
+            auto tempSourceRender = test_utilities::renderToAudioBuffer (*context->edit);
             CHECK (test_utilities::getRMSLevel (tempSourceRender, { 4_tp, 5_tp }, 0)
                    == doctest::Approx (0.0f)); // No vol
         }
+    }
+
+    TEST_CASE("AutomationCurveList: relative")
+    {
+        auto& engine = *tracktion::engine::Engine::getEngines()[0];
+        auto context = AutomationCurveListTestContext::create (engine);
+
+        auto curveList = context->clip->getAutomationCurveList (true);
+        CHECK(curveList);
+
+        CHECK(! context->volParam->isAutomationActive());
+        auto curveMod = curveList->addCurve (*context->volParam);
+        CHECK(curveMod);
+        CHECK_EQ(curveList->getItems().size(), 1);
+
+        auto& curve = curveMod->getCurve (CurveModifierType::relative).curve;
+        curve.addPoint (2.5_bp, 0.0f, 0.0, nullptr);
+        curve.addPoint (2.5_bp, 0.25f, 0.0, nullptr); // Sharp square from 0-0.25 at 2.5b
+        CHECK(context->volParam->isAutomationActive());
+
+        context->volParam->updateStream(); //ddd shouldn't need this!
+
+        auto tempSourceRender = test_utilities::renderToAudioBuffer (*context->edit);
+        CHECK (test_utilities::getRMSLevel (tempSourceRender, { 0_tp, 2_tp }, 0)
+                == doctest::Approx (0.0f)); // First no vol
+
+        auto expectedGain = volumeFaderPositionToGain (0.25f);
+        CHECK (test_utilities::getRMSLevel (tempSourceRender, { 3_tp, 5_tp }, 0)
+                == doctest::Approx (expectedGain).epsilon (0.001f)); // Last 2s +10%
+    }
+
+    TEST_CASE("AutomationCurveList: scale")
+    {
+        auto& engine = *tracktion::engine::Engine::getEngines()[0];
+        auto context = AutomationCurveListTestContext::create (engine);
+
+        auto curveList = context->clip->getAutomationCurveList (true);
+        CHECK(curveList);
+
+        CHECK(! context->volParam->isAutomationActive());
+        auto curveMod = curveList->addCurve (*context->volParam);
+        CHECK(curveMod);
+        CHECK_EQ(curveList->getItems().size(), 1);
+
+        context->volPlugin->setVolumeDb (0.0f);
+
+        auto& curve = curveMod->getCurve (CurveModifierType::scale).curve;
+        curve.addPoint (2.5_bp, 1.0f, 0.0, nullptr);
+        curve.addPoint (2.5_bp, 0.1f, 0.0, nullptr); // Sharp square from 1-0.1 at 2.5b
+        CHECK(context->volParam->isAutomationActive());
+
+        context->volParam->updateStream(); //ddd shouldn't need this!
+
+        auto tempSourceRender = test_utilities::renderToAudioBuffer (*context->edit);
+        CHECK (test_utilities::getRMSLevel (tempSourceRender, { 0_tp, 2_tp }, 0)
+                == doctest::Approx (1.0f).epsilon (0.001f)); // First full vol
+
+        auto expectedGain = volumeFaderPositionToGain (0.1f * decibelsToVolumeFaderPosition (0.0f));
+        CHECK (test_utilities::getRMSLevel (tempSourceRender, { 3_tp, 5_tp }, 0)
+                == doctest::Approx (expectedGain).epsilon (0.001f)); // Last 2s 10%
     }
 }
 
