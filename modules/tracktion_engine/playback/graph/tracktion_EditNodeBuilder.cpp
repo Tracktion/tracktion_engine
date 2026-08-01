3b69a12037759466551985f4650751840cd16348
David Rowland <drowland96@gmail.com>
2026-08-01 10:42:10 +0100

    Renderer: Tidied formatting in the new render and loudness files

--- modules/tracktion_engine/playback/graph/tracktion_EditNodeBuilder.cpp
+++ modules/tracktion_engine/playback/graph/tracktion_EditNodeBuilder.cpp
@@ -86,7 +86,6 @@ namespace
         return false;
     }
 
-
     AudioTrack* getTrackContainingTrackDevice (Edit& edit, WaveInputDevice& device)
     {
         for (auto t : getAudioTracks (edit))
